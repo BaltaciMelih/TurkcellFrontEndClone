92f092e9e0bc52ce1295ae90a37f1d8f9cba4245
merkaya1 <110626800+merkaya1@users.noreply.github.com>
2022-11-02 20:40:00 +0300

    Odev&ders13
    Odev&ders13

---
+++ Ogrenciler/mustafaErkaya/Ders1-jsgiriş-02.11.22/app.js
@@ -0,0 +1,20 @@
+const a = 10;
+const b =4;
+
+let s ;
+
+s = a + b;
+s = a - b;
+s = a * b;
+s = a / b;
+s = a % b;
+
+s = Math.PI;
+s = Math.round( a / b); // yukarı yuvarlama
+s = Math.floor( a / b); // aşağı yuvarlama
+s = Math.PI.toFixed(2);  // son basamak kaç olsun 
+
+
+console.log(s);
+console.log(typeof s);
+
