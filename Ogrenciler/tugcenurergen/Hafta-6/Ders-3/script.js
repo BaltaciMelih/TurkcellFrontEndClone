e61a204a13e9ddf17c95f6ff5051c93aaed76bc2
Tugcenrgn <tugcenurergen9@gmail.com>
2022-11-04 19:19:54 +0300

    prompt
    get data from user

--- Ogrenciler/tugcenurergen/Hafta-6/Ders-3/script.js
+++ Ogrenciler/tugcenurergen/Hafta-6/Ders-3/script.js
@@ -1,4 +1,32 @@
 // karşılaştırma operatörleri
-a = 10;
-b = 20;
-console.log(a==b);
+// a = 10;
+// b = 20;
+// console.log(a==b);
+
+// hem türü hem değeri eşittir
+// console.log(a===b);
+
+// console.log(a!=b)
+
+// console.log(a!==b);
+
+// koşullar
+
+// a = 20;
+// b = 20;
+
+// if (a === b) {
+//   console.log("çalıştı");
+// } else {
+//   console.log("çalışmadı");
+// }
+
+
+let sayi = prompt("sayı giriniz");
+if(sayi % 2 == 0){
+    console.log("sayı çift");
+}else if(sayi % 2 == 1){
+    console.log("sayı tek");
+}else{
+    console.log("geçersiz");
+}
