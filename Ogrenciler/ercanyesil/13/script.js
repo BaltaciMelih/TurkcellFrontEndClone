5baac65fd167d4e92c2f8479cb96e8c00a696f40
Ercan <ercanyesill@outlook.com>
2022-11-08 03:06:01 +0300

    1.0.0
    bootstrap project is loaded

---
+++ Ogrenciler/ercanyesil/13/script.js
@@ -0,0 +1,37 @@
+// console.log("Javascripts 01");
+
+//Variables - Değişkenler
+// number = sayısal ifadeler
+
+// var firstNumber = 20;
+// var secondNumber = 30;
+// console.log(firstNumber + secondNumber);
+// console.log(typeof firstNumber);
+
+// String - Karakter Dizisi 
+
+// var userName = "Yesil"
+// console.log(userName);
+// console.log(typeof userName);
+
+// Boelean = Karşılaştırma değerleri (True-False)
+// var a = true;
+// console.log(a);
+// console.log(typeof a);
+
+// null = boş
+// var b = null;
+// console.log(b);
+// console.log(typeof b);
+
+// Object - Nesne 
+// var user = {
+//     name: "Ercan",
+//     age: 24
+// };
+// console.log(user);
+// console.log(typeof user);
+// console.log(user.name, user.age);
+
+s = Math.PI;
+console.log(s);
