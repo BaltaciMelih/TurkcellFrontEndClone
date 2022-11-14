22cf7798fdc181eb22c88b13c6659e4f60c1bd2b
safaksenoleem <114605881+safaksenoleem@users.noreply.github.com>
2022-11-14 19:38:32 +0300

    ders18ES6ArrowFunction

---
+++ Ogrenciler/safaksenol/ders18-js-sessionstorage/ES6/ArrowFunction/app.js
@@ -0,0 +1,25 @@
+// function deneme(name) {
+//     console.log("Merhaba" + name);
+// };
+
+// deneme("Şafak");
+
+// const merhaba = function (name) {
+//     console.log("Merhaba" + name);
+// };
+// merhaba("Şafak");
+
+// const selam = (name) => console.log("Selam " + name);
+// selam("Şafak");
+
+// Küp alma işlemi
+
+// const cube = function (x) {
+//     return x * x * x;
+// }
+// console.log(cube(4));
+
+//yukarıdaki küp alma işlemini arrow functionla yapıcaz aşağıda!
+
+const cube = (x) => x * x * x;
+console.log(cube(5));
