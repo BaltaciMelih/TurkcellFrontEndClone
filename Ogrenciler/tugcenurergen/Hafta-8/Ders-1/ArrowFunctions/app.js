ea16441b9a64fe46b17e60ba58e7032f83c736dd
Tugcenrgn <tugcenurergen9@gmail.com>
2022-11-14 19:02:57 +0300

    Hafta-8
    Session Storage
    Arrow Funciton

---
+++ Ogrenciler/tugcenurergen/Hafta-8/Ders-1/ArrowFunctions/app.js
@@ -0,0 +1,35 @@
+// function deneme(name){
+//     console.log(name);
+// }
+
+// const merhaba = function (name){
+//     console.log(name);
+// }
+
+// const selam = (name) => {
+//     console.log(name);
+// }
+
+// const selam2 = name => console.log(name);
+
+// deneme("tuğçe")
+// merhaba("tuğçe")
+// selam("tuğçe")
+// selam2("tuğçe")
+
+
+const cube = function(x){
+    return x*x*x;
+}
+
+console.log(cube(4));
+
+
+const cubeArrow = (x) => {
+    return x*x*x;
+}
+
+console.log(cubeArrow(5))
+
+const cubeArrow2 = x => x*x*x;
+console.log(cubeArrow2(3));
