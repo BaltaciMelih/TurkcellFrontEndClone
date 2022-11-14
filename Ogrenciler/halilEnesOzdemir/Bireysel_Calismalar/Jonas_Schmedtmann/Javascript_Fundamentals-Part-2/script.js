bbae51dd478dc6d39acf1c13f68d46db461d992b
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-11-14 15:55:16 +0300

    Function declarations and Function Expressions differences learned

--- Ogrenciler/halilEnesOzdemir/Bireysel_Calismalar/Jonas_Schmedtmann/Javascript_Fundamentals-Part-2/script.js
+++ Ogrenciler/halilEnesOzdemir/Bireysel_Calismalar/Jonas_Schmedtmann/Javascript_Fundamentals-Part-2/script.js
@@ -12,26 +12,47 @@
 
 // FUNCTIONS
 
-function logger() {
-  console.log('My name is Halil');
-}
+// function logger() {
+//   console.log('My name is Halil');
+// }
 
 //calling / running / invoking function
-logger();
-logger();
-logger();
+// logger();
+// logger();
+// logger();
 
-function fruitProcessor(apples, oranges) {
-  console.log(apples, oranges);
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-  return juice;
+// function fruitProcessor(apples, oranges) {
+//   console.log(apples, oranges);
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+// console.log(fruitProcessor(5, 0));
+// We didn't capture the value into any variable.
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+// const num = Number('23');
+// Actually console.log and Number is built-in functions in Javascript, doing its part...
+
+// ************* Function Declarations vs. Expressions ***************
+
+// Function declaration
+
+const age1 = calcAge1(1998);
+
+function calcAge1(birthYear) {
+  return 2022 - birthYear;
 }
 
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
-console.log(fruitProcessor(5, 0)); // We didn't capture the value into any variable.
+// console.log(calcAge1(1998));
 
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
+// Function expression
+const calcAge2 = function (birthYear) {
+  return 2022 - birthYear;
+};
 
-const num = Number('23'); // Actually console.log and Number is built-in functions in Javascript, doing its part...
+console.log(age1, age2);
