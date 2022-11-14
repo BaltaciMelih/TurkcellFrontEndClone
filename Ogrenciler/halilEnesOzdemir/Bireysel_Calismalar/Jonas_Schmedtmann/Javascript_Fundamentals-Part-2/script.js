d3bb38cdbb789e3a403469f71676fc98195fcf4a
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-11-14 15:05:19 +0300

    Strict mode advantages learned and activated.

---
+++ Ogrenciler/halilEnesOzdemir/Bireysel_Calismalar/Jonas_Schmedtmann/Javascript_Fundamentals-Part-2/script.js
@@ -0,0 +1,11 @@
+// Strict mode activated...
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log('I can drive :)');
+
+// SyntaxError: Unexpected strict mode reserved word...
+const interface = 'Audio';
