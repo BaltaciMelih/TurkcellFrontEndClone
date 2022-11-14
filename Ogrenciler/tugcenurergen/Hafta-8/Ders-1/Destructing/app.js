6f00b178bdb8969758644e151ac5748273d00356
Tugcenrgn <tugcenurergen9@gmail.com>
2022-11-14 20:06:17 +0300

    spread operator

--- Ogrenciler/tugcenurergen/Hafta-8/Ders-1/Destructing/app.js
+++ Ogrenciler/tugcenurergen/Hafta-8/Ders-1/Destructing/app.js
@@ -1,23 +1,23 @@
-let number1, number2;
-let arr = [100, 200, 300, 400];
-//destructing öncesi
-// number1 = arr[0];
-// number2 = arr[1];
+// let number1, number2;
+// let arr = [100, 200, 300, 400];
+// //destructing öncesi
+// // number1 = arr[0];
+// // number2 = arr[1];
 
-//sonrası
-[number1, number2] = arr;
-console.log(number1, number2);
+// //sonrası
+// [number1, number2] = arr;
+// console.log(number1, number2);
 
-[number1, number2, ...kalanlar] = arr;
-console.log(kalanlar);
+// [number1, number2, ...kalanlar] = arr;
+// console.log(kalanlar);
 
-const numbers = {
-  a: 10,
-  b: 20,
-  c: 30,
-  d: 40,
-  e: 50,
-};
+// const numbers = {
+//   a: 10,
+//   b: 20,
+//   c: 30,
+//   d: 40,
+//   e: 50,
+// };
 
 // // const {a:n1,c:n2,e:n3} = numbers;
 // // console.log(n1 + n2 + n3); //90
