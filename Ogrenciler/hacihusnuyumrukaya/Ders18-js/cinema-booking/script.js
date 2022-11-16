474cb01e2f8d49cb73b93a303381853819c370ae
HaciHusnuYumrukaya26 <114759482+HaciHusnuYumrukaya26@users.noreply.github.com>
2022-11-16 15:21:56 +0300

    Cinema-Project

--- Ogrenciler/hacihusnuyumrukaya/Ders18-js/cinema-booking/script.js
+++ Ogrenciler/hacihusnuyumrukaya/Ders18-js/cinema-booking/script.js
@@ -5,7 +5,7 @@ const totalprice=document.getElementById("total-price");
 const select=document.getElementById("seans");
 const seats=document.querySelectorAll('.seat:not(.reserved)');
 const paybutton=document.getElementById('pay-btn');
-
+let SelectedCount=0;
 getFromLocalStorage();
 Total();
 //Eventler
@@ -19,6 +19,9 @@ function selected(e){
    if(e.target.classList.contains('seat') && !e.target.classList.contains('reserved'))
    {
     e.target.classList.toggle('selected');
+    // let ikon=document.createElement("div");
+    // ikon.innerHTML="<i class='fa-solid fa-user'></i>";
+    // e.target.appendChild(ikon);
      Total();
    }
 }
@@ -42,32 +45,67 @@ function Total(){
         return seatsArr.indexOf(seat);
     })
      console.log(selectedSeatIndexs);
-
-    let price=select.value;
-    let SelectedCount=containers.querySelectorAll('.seat.selected').length;
+     
+     let price=select.value;
+    SelectedCount=containers.querySelectorAll('.seat.selected').length;
     count.innerText="Seçilen Koltuk Sayısı: "+SelectedCount;
     totalprice.innerText=SelectedCount*price;
 
     saveLocalStorage(selectedSeatIndexs);
 }
+
 function getFromLocalStorage(){
-    const selectedSeats=JSON.parse(localStorage.getItem('selectedSeats'));
+    const selectedSeats=JSON.parse(sessionStorage.getItem('selectedSeats'));
     // const selectedMovieIndex=localStorage.getItem('selectedMovieIndex');
     if(selectedSeats!=null && selectedSeats.length>0){
         seats.forEach(function(seat, index){
             if(selectedSeats.indexOf(index)>-1)
             {
                 seat.classList.add("selected");
+                // seat.innerHTML="<i class='fa-solid fa-user'></i>";
+            }
+        })
+    }
+
+    const selectedSeats2=JSON.parse(localStorage.getItem('reservedSeats'));
+    if(selectedSeats2!=null && selectedSeats2.length>0){
+        seats.forEach(function(seat, index){
+            if(selectedSeats2.indexOf(index)>-1)
+            {
+                seat.classList.add("reserved");
+                SelectedCount=0;
+                count.innerText="Seçilen Koltuk Sayısı: 0";
+                totalprice.innerText="0";
             }
         })
     }
 }
 
 function saveLocalStorage(indexs){
-    localStorage.setItem('selectedSeats',JSON.stringify(indexs));
-    localStorage.setItem('selectedMovieIndex',select.selectedIndex);
+    sessionStorage.setItem('selectedSeats',JSON.stringify(indexs));
+    sessionStorage.setItem('selectedMovieIndex',select.selectedIndex);
 }
 
 function payButtons(){
-    const seatsArr=getFromLocalStorage();
+    let ıtems = JSON.parse(sessionStorage.getItem("selectedSeats"));
+    localStorage.setItem('reservedSeats',JSON.stringify(ıtems));
+    localStorage.setItem('reservedMovieIndex',select.selectedIndex);
+    const selectedSeats=JSON.parse(sessionStorage.getItem('selectedSeats'));
+    // const selectedMovieIndex=localStorage.getItem('selectedMovieIndex');
+    if(selectedSeats!=null && selectedSeats.length>0){
+        seats.forEach(function(seat, index){
+            if(selectedSeats.indexOf(index)>-1)
+            {
+                seat.classList.add("reserved");
+                // seat.innerHTML="";
+                 sessionStorage.removeItem("selectedSeats");
+                SelectedCount=0;
+                count.innerText="Seçilen Koltuk Sayısı: 0";
+                totalprice.innerText="0";
+            }
+        })
+    }
+
 }
+
+
