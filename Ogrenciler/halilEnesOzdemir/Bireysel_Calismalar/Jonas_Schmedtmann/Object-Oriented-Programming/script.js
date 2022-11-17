c33b9122226e54280c0a8f0438e9f1bcd4eed499
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-11-17 14:03:44 +0300

    Constructor Functions and the new Operator topic handled.

---
+++ Ogrenciler/halilEnesOzdemir/Bireysel_Calismalar/Jonas_Schmedtmann/Object-Oriented-Programming/script.js
@@ -0,0 +1,28 @@
+'use strict';
+// Arrow function will actully not work as a function constructor.(This keyword...)
+
+const Person = function (firstName, birthYear) {
+  // Instance properties...
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never to this... (BAD PRACTISE) -> SOLVE : Protoype Inheritance...
+  this.calcAge = function () {
+    console.log(2037 - this.birthYear);
+  };
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log();
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person); // Check jonas is instance..
+
+// 1. New {} is created
+// 2. Function is called, this = {};
+// 3. {} linked to prototype
+// 4. function automatically return {}
