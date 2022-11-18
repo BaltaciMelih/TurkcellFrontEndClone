89ad27970bff729be0ee89d8a1df728210d00141
Süleyman Emre Karabay <karabayemre10@gmail.com>
2022-11-18 19:19:51 +0300

    uygulama_sinema_last_update

--- Ogrenciler/suleymanEmreKarabay/uygulama_sinema/app.js
+++ Ogrenciler/suleymanEmreKarabay/uygulama_sinema/app.js
@@ -2,6 +2,10 @@ const koltukNumaralari = document.querySelectorAll(".koltuk");
 const satinAlButton = document.querySelector("#satinAl");
 const REZERV_KOLTUKLAR_KEY = "rezerveKoltuklar";
 const SATIN_ALINAN_KOLTUKLAR_KEY = "satinAlinanKoltuklar";
+let bilgiDiv = document.querySelector(".bilgi");
+let ucretDiv = document.querySelector(".toplamUcret");
+const BILET_UCRETI = 25;
+let filmSecimi = document.querySelector("#filmSecimi");
 
 eventListeners();
 function eventListeners() {
@@ -14,29 +18,74 @@ function eventListeners() {
     "DOMContentLoaded",
     getSatinAlinanKoltuklarFromStorage
   );
-  document.addEventListener("DOMContentLoaded", satinAlinanKoltuklar);
-  document.addEventListener("DOMContentLoaded", rezerveEdilenKoltuklar);
+  filmSecimi.addEventListener("change", filmSecimiFonks);
+  filmSecimi.addEventListener("change", satinAlinanKoltuklar);
+  document.addEventListener("change", rezerveEdilenKoltuklar);
+}
+
+function filmSecimiFonks() {
+  localStorage.setItem("film", filmSecimi.value);
+  rezervRenkSil();
+  setRezervKoltuklar([]);
+}
+
+function bilgiYazdirma() {
+  let rezerveEdilenKoltuklar = getRezerveKoltuklarFromStorage();
+  bilgiDiv.innerText = "Rezerve:";
+  bilgiDiv.innerText += rezerveEdilenKoltuklar.toString();
+  ucretDiv.innerText = "Toplam:";
+  ucretDiv.innerText += rezerveEdilenKoltuklar.length * BILET_UCRETI;
+  ucretDiv.innerText += " TL";
+}
+
+function bilgiSildirme() {
+  let rezerveEdilenKoltuklar = getRezerveKoltuklarFromStorage();
+  if (rezerveEdilenKoltuklar.length > 0) {
+    bilgiYazdirma();
+  } else {
+    bilgiDiv.innerText = "";
+    ucretDiv.innerText = "";
+  }
 }
 
 function satinAl() {
+  let film = localStorage.getItem("film");
+  let keyAdi = "satinAlinanKoltuklar" + film;
+
   let rezerveEdilenKoltuklar = getRezerveKoltuklarFromStorage();
-  let satinAlinanKoltuklar = getSatinAlinanKoltuklarFromStorage();
+  let satinAlinanKoltuklarDizisi = getSatinAlinanKoltuklarFromStorage(keyAdi);
 
   rezerveEdilenKoltuklar.forEach((element) => {
-    satinAlinanKoltuklar.push(element);
+    satinAlinanKoltuklarDizisi.push(element);
   });
-  setSatınAlınanKoltuklar(satinAlinanKoltuklar);
+  rezervRenkSil();
+  setSatınAlınanKoltuklar(satinAlinanKoltuklarDizisi);
   rezerveEdilenKoltuklar = [];
   setRezervKoltuklar(rezerveEdilenKoltuklar);
   satinAlinanKoltuklar();
+  bilgiSildirme();
+}
+
+function rezervRenkSil() {
+  let rezerveEdilenKoltuklar = getRezerveKoltuklarFromStorage();
+
+  koltukNumaralari.forEach((element) => {
+    rezerveEdilenKoltuklar.forEach((element2) => {
+      if (element.value === element2) {
+        element.classList.remove("bg-danger");
+      }
+    });
+  });
 }
 
 function rezerveEt(e) {
+  console.log(e);
   let rezerveEdilenKoltuklar = getRezerveKoltuklarFromStorage();
   if (!rezerveEdilenKoltuklar.includes(e.target.value)) {
     e.target.className += " bg-danger";
     rezerveEdilenKoltuklar.push(e.target.value);
     setRezervKoltuklar(rezerveEdilenKoltuklar);
+    bilgiYazdirma();
   } else {
     e.target.classList.remove("bg-danger");
     const index = rezerveEdilenKoltuklar.indexOf(e.target.value);
@@ -44,6 +93,7 @@ function rezerveEt(e) {
       rezerveEdilenKoltuklar.splice(index, 1);
     }
     setRezervKoltuklar(rezerveEdilenKoltuklar);
+    bilgiSildirme();
   }
 }
 
@@ -52,35 +102,43 @@ function setRezervKoltuklar(rezervKoltuklar) {
 }
 
 function setSatınAlınanKoltuklar(satınAlınanKoltuklar) {
-  localStorage.setItem(
-    SATIN_ALINAN_KOLTUKLAR_KEY,
-    JSON.stringify(satınAlınanKoltuklar)
-  );
+  let film = localStorage.getItem("film");
+  let keyAdi = "satinAlinanKoltuklar" + film;
+  localStorage.setItem(keyAdi, JSON.stringify(satınAlınanKoltuklar));
 }
 
 function satinAlinanKoltuklar() {
-  console.log("çalıştı");
-  const satinAlinanKoltuklar = getSatinAlinanKoltuklarFromStorage();
+  let film = localStorage.getItem("film");
+  let keyAdi = "satinAlinanKoltuklar" + film;
+  const satinAlinanKoltuklar = getSatinAlinanKoltuklarFromStorage(keyAdi);
+  console.log(satinAlinanKoltuklar);
   setSatınAlınanKoltuklar(satinAlinanKoltuklar);
-  koltukNumaralari.forEach((element) => {
-    satinAlinanKoltuklar.forEach((element2) => {
-      if (element.value === element2) {
-        element.className += "bg-info border-0 disabled";
+  butunKoltuklarBos();
+  satinAlinanKoltuklar.forEach((element) => {
+    koltukNumaralari.forEach((element2) => {
+      if (element2.value === element) {
+        element2.className += " bg-info border-0 disabled";
       }
     });
   });
+
+  bilgiYazdirma();
+  bilgiSildirme();
 }
 
-function getSatinAlinanKoltuklarFromStorage() {
-  let satinAlinanKoltuklar;
-  if (localStorage.getItem("satinAlinanKoltuklar") === null) {
-    satinAlinanKoltuklar = [];
+function butunKoltuklarBos() {
+  koltukNumaralari.forEach((element) => {
+    element.classList.remove("bg-info", "border-0", "disabled");
+  });
+}
+
+function getSatinAlinanKoltuklarFromStorage(key) {
+  if (localStorage.getItem(key) === null) {
+    key = [];
   } else {
-    satinAlinanKoltuklar = JSON.parse(
-      localStorage.getItem("satinAlinanKoltuklar")
-    );
+    key = JSON.parse(localStorage.getItem(key));
   }
-  return satinAlinanKoltuklar;
+  return key;
 }
 
 function rezerveEdilenKoltuklar() {
