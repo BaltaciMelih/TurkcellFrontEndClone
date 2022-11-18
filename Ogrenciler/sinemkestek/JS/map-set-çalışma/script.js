24941a76ac2252aac47289e7ef4d2c93175b71f4
SinemKestek <sinemkestekkkkk@hotmail.com>
2022-11-18 13:58:29 +0300

    Set-example

---
+++ Ogrenciler/sinemkestek/JS/map-set-çalışma/script.js
@@ -0,0 +1,48 @@
+//Set
+//value değerleri unique olmalı.
+//key=value
+/*
+methods:
+-add(value):element ekler.
+ -size:element uzunluğu
+-has(value)
+-delete(value)
+-clear:tüm value değerleri silinir.
+*/
+// let set = new Set();
+// let john = { name: "John" };
+// let sinem = { name: "Sinem" };
+
+// let ertugrul = { name: "Ertugrul" };
+// set.add(john);eleman ekleme
+// set.add(sinem);
+// set.add(ertugrul);
+// set.add(sinem); /*value eklenmez */
+// console.log(set);
+// console.log(set.size);
+//set.clear();set içerisindeki tüm değerleri siler.
+//set.delete();alınan parametreye göre değeri siler.
+
+// for (let user of set) {
+//   console.log(user.name);
+// }
+//set.forEach(function(value){
+// console.log(value)} )
+//eleman değerlerini tek sefer döndürür.
+
+// let fruits = ["watermelon", "lemon", "apple", "lemon", "watermelon", "cherry"];
+// alert(unique(fruits));
+
+// function unique(arr) {
+//   return Array.from(new Set(arr));
+// } // new Set() ile array içerisindeki tekrar eden değerleri yazmaz//
+//set.has("watermelon")--True/False döner.
+/*
+// intersect -ortak değer kesişim
+const intersection = new Set([...mySet1].filter((x) => mySet2.has(x)));
+
+
+// difference 
+const difference = new Set([...mySet1].filter((x) => !mySet2.has(x)));
+
+ */
