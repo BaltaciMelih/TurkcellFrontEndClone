306ab22238bb0fe872cc457636ff12fde9137ca9
safaksenoleem <114605881+safaksenoleem@users.noreply.github.com>
2022-11-22 12:03:15 +0300

    ders21jstodooop

---
+++ Ogrenciler/safaksenol/ders21-js/todo-js-oop/project.js
@@ -0,0 +1,17 @@
+const ui = new UI();
+
+const clearButton = document.getElementById("clear-todos");
+
+
+eventListeners();
+function eventListeners() {
+  clearButton.addEventListener("click", clearAllTodos);
+ 
+}
+
+function clearAllTodos() {
+  if (confirm("Emin misiniz?")) {
+    ui.displayMessage("Silme işlemi başarılı", "success");
+    ui.clearAllTodosUI();
+  }
+}
---
+++ Ogrenciler/safaksenol/ders21-js/todo-js-oop/ui.js
@@ -0,0 +1,32 @@
+function UI() {}
+
+UI.prototype.addTodoToUI = function (newTodo) {
+  const todoList = document.getElementById("todo-list");
+  todoList.innerHTML += `
+  <li>
+    <p>${newTodo}</p>
+  </li>
+  `;
+};
+
+UI.prototype.clearInput = function (e) {
+  e.value = "";
+};
+
+UI.prototype.displayMessage = function (message, type) {
+  const cardBody = document.querySelector(".card-body");
+  const div = document.createElement("div");
+  div.className = `alert alert-${type}`;
+  div.textContent = message;
+  cardBody.appendChild(div);
+  setTimeout(function () {
+    div.remove();
+  }, 2000);
+};
+
+UI.prototype.clearAllTodosUI = () => {
+  const todoList = document.getElementById("todo-list");
+  while(todoList.firstChild != null){
+    todoList.removeChild(todoList.firstChild); 
+  }
+};
