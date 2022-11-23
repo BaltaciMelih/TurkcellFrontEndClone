d612fb0b43d2d3cced8331637eac6b526dcd2483
SinemKestek <sinemkestekkkkk@hotmail.com>
2022-11-23 15:25:45 +0300

    proje değişiklik

--- Ogrenciler/sinemkestek/Odevler/Odev-2/uı.js
+++ Ogrenciler/sinemkestek/Odevler/Odev-2/uı.js
@@ -4,32 +4,59 @@ UI.prototype.showFilms = function (filmForShow) {
   filmForShow.map((film, index) => {
     return (htmlTemplate =
       htmlTemplate +
-      `<tr class="movie-container">
+      `<tr id=${index} class="movie-container">
                 <td><img class="img-thumbnail" style="width:130px"class="border-bottom bottom-2" src="${film.bannerUrl}" alt="banner-${index}"></td>
                 <td>${film.name}</td>
                 <td>${film.director}</td>
                 <td>${film.year}</td>
                 <td>
-                <button class="btn btn-dark">Kaldır</button>
+                <button class="btn btn-dark" onClick="deletMovie(${index})">Kaldır</button>
                 <button class="btn btn-primary">Düzenle</button>
                 </td>
                 </tr> `);
   });
   return (tableBodyEl.innerHTML = htmlTemplate);
 };
+
+UI.prototype.addFilms = function (filmForShow) {
+  let htmlTemplate = tableBodyEl.innerHTML;
+
+  let filmsLength = JSON.parse(localStorage.getItem("films")).length;
+  htmlTemplate += `<tr id=${filmsLength} class="movie-container">
+  <td><img class="img-thumbnail" style="width:130px"class="border-bottom bottom-2" src="${filmForShow.bannerUrl}" alt="banner-${filmsLength}"></td>
+  <td>${filmForShow.name}</td>
+  <td>${filmForShow.director}</td>
+  <td>${filmForShow.year}</td>
+  <td>
+  <button class="btn btn-dark" onClick="deletMovie(${filmsLength})">Kaldır</button>
+  <button class="btn btn-primary">Düzenle</button>
+  </td>
+  </tr> `;
+  return (tableBodyEl.innerHTML = htmlTemplate);
+};
+
 UI.prototype.displayMessage = function (message, type) {
-  const formContainer = document.querySelector(".form-container");
+  const formContainer = document.querySelector("body");
   const div = document.createElement("div");
   div.className = `alert alert-${type}`;
   div.textContent = message;
-  formContainer.appendChild(div);
-  setTimeout(function () {
-    div.remove();
-  }, 3000);
+  if (!document.querySelector(".alert")) {
+    formContainer.appendChild(div);
+    setTimeout(function () {
+      div.remove();
+    }, 3000);
+  }
 };
-
+//input sıfırlama
+UI.prototype.valuesElEmpty = function () {
+  filmNameEl.value = "";
+  filmDirectorEl.value = "";
+  filmYearEl.value = "";
+  filmBannerEl.value = "";
+};
 UI.prototype.clearFilms = function () {
   films = [];
   localStorage.removeItem("films");
   tableBodyEl.innerHTML = "";
 };
+//düzenle
