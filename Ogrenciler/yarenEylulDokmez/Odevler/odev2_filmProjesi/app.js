7783e4ecea045d43ae74de0aa97c25f221e5be32
yareneylul <eyluldokmez@gmail.com>
2022-11-23 16:12:39 +0300

    js-filmlistOdevi

---
+++ Ogrenciler/yarenEylulDokmez/Odevler/odev2_filmProjesi/app.js
@@ -0,0 +1,70 @@
+const ui = new UI();
+const storage = new Storage();
+let filmList = document.getElementById("filmler");
+let filmName = document.getElementById("filmName");
+let filmDirector = document.getElementById("filmDirector");
+let filmDate = document.getElementById("filmDate");
+let filmUrl = document.getElementById("filmBanner");
+let addFilm = document.getElementById("film-add");
+let clearAll = document.getElementById("delete-all");
+let films = [];
+
+eventListeners();
+function eventListeners() {
+  document.addEventListener("DOMContentLoaded", defaultFilms);
+  addFilm.addEventListener("click", addMovie);
+  clearAll.addEventListener("click", ui.clearFilms);
+  filmList.addEventListener('click', deleteFilm);
+}
+
+function defaultFilms() {
+  films = storage.defaultMovie();
+  if (films.length < 1) {
+    const dFilms = [
+      new Film(
+        "Avengers 1",
+        "Joss Whedon",
+        "04/05/2012",
+        "https://tr.web.img3.acsta.net/medias/nmedia/18/85/49/05/20085945.jpg"
+      ),
+      new Film(
+        "Avengers 2",
+        "Joss Whedon",
+        "01/05/2015",
+        "https://tr.web.img4.acsta.net/pictures/15/08/06/07/51/269807.jpg"
+      ),
+    ];
+    films = dFilms;
+    localStorage.setItem("films", JSON.stringify(dFilms));
+  }
+  ui.addTodoToUI(films);
+
+}
+
+function addMovie() {
+  let name = filmName.value;
+  let director = filmDirector.value;
+  let date = filmDate.value;
+  let url = filmUrl.value;
+  let newFilm = [];
+  if (name === "" || director === "" || date == "" || url === "") {
+    ui.displayMessage("Tüm alanları doldurunuz", "danger");
+  } else {
+  newFilm.push(new Film(name, director, date,url));
+  films.push(newFilm[0]);
+  ui.addTodoToUI(newFilm);
+  filmName.value = "";
+  filmDirector.value = "";
+  filmDate.value = "";
+  filmUrl.value = "";
+  localStorage.setItem("films", JSON.stringify(films));
+  ui.displayMessage("Film Başarıyla Yüklendi","success");
+};
+};
+
+function deleteFilm(e) {
+    ui.deleteMovie(e.target);
+    storage.deleteMovie(e.target.parentElement.parentElement.previousElementSibling.previousElementSibling.previousElementSibling
+      .textContent);
+     ui.displayMessage("Silme işlemi gerçekleşti", "success");
+  };
---
+++ Ogrenciler/yarenEylulDokmez/ders15/todo/app.js
@@ -0,0 +1,104 @@
+// Elemanları Seçme
+const form = document.getElementById("todo-form");
+const todoInput = document.getElementById("todo");
+const todoList = document.querySelector(".list-group");
+const clearButton = document.getElementById("clear-todos");
+const firstCardBody = document.querySelectorAll(".card-body")[0];
+const secondCardBody = document.querySelectorAll(".card-body")[1];
+const filter = document.getElementById("filter");
+
+// Eventler
+eventListeners();
+function eventListeners() {
+  form.addEventListener("submit", addTodo);
+  clearButton.addEventListener("click", clearAllTodos);
+  secondCardBody.addEventListener("click", deleteTodo);
+  filter.addEventListener("keyup", filterTodos);
+}
+
+// Todo Ekleme
+function addTodo(e) {
+  const newTodo = todoInput.value.trim();
+  if (newTodo === "") {
+    showAlert("danger", "Lütfen bir todo giriniz.");
+  } else {
+    addTodoToUI(newTodo);
+    showAlert("success", "Todo başarılı bir şekilde eklendi.");
+  }
+  e.preventDefault();
+}
+
+// Todo Ekleme (UI Tarafını Oluşturma)
+function addTodoToUI(newTodo) {
+  const listItem = document.createElement("li");
+  const link = document.createElement("a");
+  link.href = "#";
+  link.className = "delete-item";
+  link.innerHTML = "<i class='fa fa-remove'></i>";
+  listItem.className = "list-group-item d-flex justify-content-between";
+  listItem.appendChild(document.createTextNode(newTodo));
+  listItem.appendChild(link);
+  todoList.appendChild(listItem);
+  todoInput.value = "";
+}
+
+// ütün todoları silme
+function clearAllTodos() {
+  // todoList.innerHTML = "";
+  while (todoList.firstChild != null) {
+    todoList.removeChild(todoList.firstChild); // Daha hızlı çalışacak
+  }
+}
+
+// Delete Todo (tek tek silme işlemi)
+function deleteTodo(e) {
+  // console.log(e.target);
+  // console.log(e.target.parentElement);
+  // console.log(e.target.parentElement.parentElement);
+  if (e.target.className === "fa fa-remove") {
+    e.target.parentElement.parentElement.remove();
+    console.log("todo başarıyla silindi");
+  }
+}
+
+// Filtreleme
+function filterTodos(e) {
+  const filterValue = e.target.value.toLowerCase();
+  const listItems = document.querySelectorAll(".list-group-item");
+  // console.log(listItems);
+  listItems.forEach(function (listItem) {
+    const text = listItem.textContent.toLowerCase();
+    console.log(text.indexOf(filterValue));
+    if (text.indexOf(filterValue) === -1) {
+      const alert = document.querySelector(".alert");
+      if (!alert) {
+        showAlert("warning", "Aradığınız Todo Bulunamadı!");
+      }
+      listItem.setAttribute("style", "display:none !important");
+    } else {
+      listItem.setAttribute("style", "display:block");
+    }
+  });
+}
+
+// Alert Metodu
+function showAlert(type, message) {
+  const alert = document.createElement("div");
+  alert.className = `mt-3 alert alert-${type}`;
+  alert.textContent = message;
+  firstCardBody.appendChild(alert);
+
+  setTimeout(function () {
+    alert.remove();
+  }, 2000);
+}
+
+
+//storage ekleme
+// localStorage.setItem("deneme","içerik");
+// const value= localStorage.getItem("deneme");//içerik verir
+// console.log(value);
+
+// //silme
+// localStorage.clear();
+
