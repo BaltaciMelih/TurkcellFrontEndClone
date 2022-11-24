831fa81736658ccdbff5096dfc9ee8ea1d7c9306
okanmaden <99764271+okanmaden@users.noreply.github.com>
2022-11-24 23:48:15 +0300

    Odev-02-Prototype and Odev-02-Class Done.
    -Edit button working way is changed.
    -File edition for prototype and class parts.
    -responsive part done.
    -movie cards used instead ul and li elements for movies.
    - need to work for window.scroll for editing movie.

---
+++ Ogrenciler/okanresitmaden/Odevler/Odev-02/Odev02-Prototype/storage.js
@@ -0,0 +1,11 @@
+function Store(){
+
+}
+
+Store.prototype.addToStorage = function(movies){
+    localStorage.setItem("movies", JSON.stringify(movies));
+}
+
+Store.prototype.GetMovies = function(){
+    return JSON.parse(localStorage.getItem("movies"));
+}
