0ae4d81055ed5ef2177b964b3a805d1d7ce599c2
Tugcenrgn <tugcenurergen9@gmail.com>
2022-11-24 20:53:40 +0300

    update

---
+++ Ogrenciler/tugcenurergen/Odevler/MovieApp-2/domElements.js
@@ -0,0 +1,30 @@
+class DomElements{
+    static getDoms(){
+        const uploadBox = document.querySelector(".upload-box");
+        const fileInput = uploadBox.querySelector("input");
+        const previewImg = uploadBox.querySelector("img");
+        const urlBox = document.querySelector(".url-box");
+        const enterUrlInput = urlBox.querySelector("#enter-url");
+        const movieName = document.querySelector("#movie-name");
+        const movieDirector = document.querySelector("#movie-director");
+        const movieDate = document.querySelector("#movie-date");
+        const saveButton = document.querySelector(".save-button");
+        const list = document.querySelector(".movies");
+        const deleteButton = document.querySelector(".delete-btn");
+        return {
+            uploadBox,
+            fileInput,
+            previewImg,
+            urlBox,
+            enterUrlInput,
+            movieName,
+            movieDirector,
+            movieDate,
+            saveButton,
+            list,
+            deleteButton,
+        };
+}
+}
+
+export default class {DomElements};
