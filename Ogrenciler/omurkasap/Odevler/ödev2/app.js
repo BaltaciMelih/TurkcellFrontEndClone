562db517bd6b9c26df219e06264c8804e5b5b026
omurkkasap <omurkkasap@users.noreply.github.com>
2022-11-26 01:19:51 +0300

    odev2 prototype
    Filmleri locale alma ve silme değişiklikleri yapıldı. Bazı Alert fonksiyonları verildi. Emekleri için Mustafa'ya teşekkürler :)

--- Ogrenciler/omurkasap/Odevler/ödev2/app.js
+++ Ogrenciler/omurkasap/Odevler/ödev2/app.js
@@ -1,5 +1,6 @@
 const ui = new Ui();
 const film = new Film();
+const storage = new Storage();
 
 
 const inputName = document.querySelector("#name")
@@ -8,27 +9,22 @@ const inputİmg = document.querySelector("#img")
 const inputDate = document.querySelector("#date")
 const btn = document.querySelector("#btnCreate")
 const tbody = document.querySelector(".tbody");
-
-
+const alerts = document.querySelector(".uyari")
+const deleteAllbtn = document.querySelector("#deleteAll")
 
 function eventListeners() {
     btn.addEventListener("click", ui.addMovie)
     tbody.addEventListener("click", ui.dltMovie)
+    deleteAllbtn.addEventListener("click",ui.deleteAll)
+    document.addEventListener("DOMContentLoaded", load);
 } 
 eventListeners();
+function load (){
+    storage.getFromStorage();
+    storage.movies.forEach(function(item){
+        ui.addMovieUi(item)
+    })
+};
 
-function getLs(){
-    let movie;
 
-    if (localStorage.getItem("film")===null){
-        movie=[]
-    }
-    else {
-        movie= JSON.parse(localStorage.getItem("film"))
-        console.log(movie)
-    }
-
-    ui.addMovieUi({movie})
-}
-getLs();
 
--- Ogrenciler/omurkasap/Odevler/ödev2/ui.js
+++ Ogrenciler/omurkasap/Odevler/ödev2/ui.js
@@ -1,9 +1,8 @@
-
 function Ui() {
-
+    
 
 }
-let films = []
+
 Ui.prototype.addMovie = function(){
     const name = inputName.value.trim();
     console.log(name)
@@ -11,10 +10,16 @@ Ui.prototype.addMovie = function(){
     const img = inputİmg.value.trim();
     const date = inputDate.value.trim();
     const film = new Film(name, director, date, img);
+
     ui.addMovieUi(film)
-    films.push(film)
-    console.log(films)
-    localStorage.setItem("film",JSON.stringify(films))
+    //films.push(film)
+    storage.movies.push(film)
+    console.log(storage.movies)
+    storage.setFromStorage();
+    ui.alert("Film Eklendi", "success")
+    ui.resetForm();
+    
+    // localStorage.setItem("film",JSON.stringify(films))
 }
 Ui.prototype.addMovieUi = function({name,director,date,img}){
     const tr = document.createElement("tr");
@@ -31,13 +36,55 @@ Ui.prototype.addMovieUi = function({name,director,date,img}){
   tbody.prepend(tr);
 }
 Ui.prototype.dltMovie = function(e){
-    console.log("e", e)
+    // console.log("e", e)
     if (e.target.id==="delete"){
 
         e.target.parentElement.parentElement.remove();
         console.log("SİLİNDİ")
+        ui.alert("Film Başarı ile Silindi","primary")
+
     }
     else {
-        console.log("Butona Tıklamadınız!!")
+        //console.log("Butona Tıklamadınız!!")
+        ui.alert("Silme Butonu Tıklanmadı","danger")
     }
+    let title = ui.movieTitle(e);
+    storage.updateStorage(title);
+    
+}
+
+Ui.prototype.alert = function(message, type){
+    const div = document.createElement("div");
+    div.className = `mt-3 alert alert-${type}`;
+    div.textContent = message;
+    alerts.insertBefore(div,alerts.children[1])
+    setTimeout(function(){
+        div.remove()
+
+    }, 2500);
+}
+
+Ui.prototype.resetForm = function(){
+    inputİmg.value = "";
+    inputDate.value = "";
+    inputDirector.value = "";
+    inputName.value = "";
+}
+Ui.prototype.deleteAll = function(){
+    if (confirm("Emin Misiniz?")){
+        while(tbody.firstChild!==null){
+            tbody.removeChild(tbody.firstChild)
+        }
+        ui.alert("Hepsi Silindi","danger")
+        localStorage.clear();
+    }
+    console.log("Hepsini sil")
+
+}
+
+Ui.prototype.movieTitle = function(e){
+    let title = e.target.parentElement.parentElement.children[1].textContent.trim();
+    return title;
+    // console.log(title)
+    // console.log(typeof title)
 }
