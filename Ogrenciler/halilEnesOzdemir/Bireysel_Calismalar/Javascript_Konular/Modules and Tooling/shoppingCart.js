157a469cf04d92215cf709e2a14bd275fb57a74c
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-11-25 18:28:44 +0300

    JS Modules, Exporting and Importing in ES6

---
+++ Ogrenciler/halilEnesOzdemir/Bireysel_Calismalar/Javascript_Konular/Modules and Tooling/shoppingCart.js
@@ -0,0 +1,29 @@
+// Exporting Module
+console.log('Exporting module');
+
+const shippingCost = 10;
+export const cart = [];
+
+//  This is not work... EXPORT ---> HAVE TO TOP LEVEL...
+
+/* if (true) {
+  export const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
+} */
+
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity as tq };
+
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+}
