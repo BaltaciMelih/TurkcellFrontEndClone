f267a4ad0b238ad8754cd1d381191fc57327a39b
Varol Maksutoglu <varolmaksutoglu@yandex.com>
2022-11-28 22:15:17 +0300

    Odev - 3

--- Dersler/Ders23-JS-Promise-Fetch-Async/04-Json-Server/app.js
+++ Dersler/Ders23-JS-Promise-Fetch-Async/04-Json-Server/app.js
@@ -1,17 +1,72 @@
 class Request {
-  async get(url) {
-    const res = await fetch(url);
-    if (!res.ok) {
-      throw new Error(`Bir hata oluştu ${res.status}`);
-    }
-    const data = await res.json();
-    return data;
+  get(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    });
+  }
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((response) => response.json())
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    });
+  }
+  put(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "PUT",
+        body: JSON.stringify(data),
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((response) => response.json())
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    });
+  }
+  delete(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "DELETE",
+      })
+        .then((response) => resolve("Veri Silme Başarılı", response))
+        .catch((err) => reject(err));
+    });
   }
 }
 
 const request = new Request();
 
+// request
+//   .get("http://localhost:3000/users")
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
 request
-  .get("http://localhost:3000/posts")
+  .post("http://localhost:3000/users", {
+    id: 4,
+    name: "postDeneme",
+    nick: "Merhaba",
+  })
   .then((data) => console.log(data))
-  .catch((err) => console.log(`Hata: ${err.message}`));
+  .catch((err) => console.log(err));
+
+// request
+//   .put("https://jsonplaceholder.typicode.com/albums/1", {
+//     userId: "9999",
+//     title: "Deneme Put",
+//   })
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
+// request
+//   .delete("https://jsonplaceholder.typicode.com/albums/1")
+//   .then((msg) => console.log(msg))
+//   .catch((err) => console.log(err));
