33a170f50adc88849323a8ded1171cbf4f90a4ab
Tugcenrgn <tugcenurergen9@gmail.com>
2022-11-28 20:07:18 +0300

    fetch with class

--- Ogrenciler/tugcenurergen/Hafta-10/Ders-1-Promise-Fetch-Async/02-Fetch/app.js
+++ Ogrenciler/tugcenurergen/Hafta-10/Ders-1-Promise-Fetch-Async/02-Fetch/app.js
@@ -1,18 +1,80 @@
-function getTextFile(){
+// function getTextFile(){
 
-  fetch("ornek.txt") //get isteği //fetch aslında bize promise döner
-    .then((response) => response.text()) //response text i retrun etmiş olduk
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
+//   fetch("ornek.txt") //get isteği //fetch aslında bize promise döner
+//     .then((response) => response.text()) //response text i retrun etmiş olduk
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err))
+// }
+// getTextFile();
+
+// function getJsonFile(){
+
+//   fetch("users.json") //get isteği //fetch aslında bize promise döner
+//     .then((response) => response.json()) //response text i retrun etmiş olduk
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err))
+// }
+// getJsonFile();
+
+// function getApi(){
+//   fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+// }
+
+// getApi();
+
+class Request {
+  get(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    });
+  }
+
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "POST",
+        urlbody: JSON.stringify(data),
+        headers: { "Content-type": "application:json" },
+      })
+        .then((response) => response.json())
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    });
+
+  }
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "PUT",
+        urlbody: JSON.stringify(data),
+        headers: { "Content-type": "application:json" },
+      })
+        .then((response) => response.json())
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    });
+
+  }
+  delete(url){
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method:"DELETE"
+      })
+      .then(response => resolve("Veri silme başarılı", response))
+      .catch(err => reject(err))
+    })
+  }
+
 }
-getTextFile();
 
 
-function getJsonFile(){
-
-  fetch("users.json") //get isteği //fetch aslında bize promise döner
-    .then((response) => response.json()) //response text i retrun etmiş olduk
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
-}
-getJsonFile();
+const request = new Request //static yazmadığımız için yeni obje oluşturup kullanmalıyız
+request.get("https://jsonplaceholder.typicode.com/albums")
+.then((data) => console.log(data))
+.catch((err) => console.log(err));
