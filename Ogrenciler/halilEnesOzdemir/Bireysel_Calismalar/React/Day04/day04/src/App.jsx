e948458be4fd26f69a3acb82fd966c718bcd39f5
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-11-28 11:13:32 +0300

    Input state sample completed, push method and spread differences understood.

---
+++ Ogrenciler/halilEnesOzdemir/Bireysel_Calismalar/React/Day04/day04/src/App.jsx
@@ -0,0 +1,11 @@
+import InputStateSample from './StateSample/InputStateSample';
+
+function App() {
+  return (
+    <>
+      <InputStateSample></InputStateSample>
+    </>
+  );
+}
+
+export default App;
