d59c181560453d66ed560de39e0d5d9458cb7b05
Süleyman Emre Karabay <karabayemre10@gmail.com>
2022-11-28 19:20:17 +0300

    ders22_uygulama_update

--- Ogrenciler/suleymanEmreKarabay/ders22_request/api_uygulama/app.js
+++ Ogrenciler/suleymanEmreKarabay/ders22_request/api_uygulama/app.js
@@ -1,8 +1,44 @@
 let thumbnailUrl = document.getElementById("thumbnailUrl");
 let title = document.getElementById("title");
 let rowDiv = document.getElementById("rowDiv");
-
+let containerDiv = document.getElementById("containerDiv");
+containerDiv.innerHTML = "";
 document.addEventListener("DOMContentLoaded", getAPI);
+
+const KEY_NAME = "photos";
+
+function toggleOpen(e) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://jsonplaceholder.typicode.com/photos");
+  xhr.onload = function () {
+    if (this.status) {
+      const response = JSON.parse(this.responseText);
+
+      containerDiv.innerHTML = `<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" style="width:"600px";height:"800px"">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">${
+              response[e.target.parentElement.id - 1].title
+            }</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+          <img src="${response[e.target.parentElement.id - 1].url}" alt="url">
+
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>`;
+    }
+  };
+  xhr.send();
+  console.log(e.target.parentElement.id);
+}
 
 function getAPI() {
   const xhr = new XMLHttpRequest();
@@ -12,16 +48,38 @@ function getAPI() {
     if (this.status) {
       const response = JSON.parse(this.responseText);
       for (let i = 0; i < 12; i++) {
+        let kapsayıcıDiv = document.createElement("div");
+        kapsayıcıDiv.className = "col-sm-2";
+
         let cardDiv = document.createElement("div");
-        cardDiv.className = "col-sm-2";
-        cardDiv.innerHTML = `
-        <div class="card mb-2">
-            <img src="${response[i].thumbnailUrl}" class="card-img-top" alt="thumbnailUrl" id="thumbnailUrl">
-            <div class="card-body">
-              <h5 class="card-title" id="title">${response[i].title}</h5>
-            </div>
-        </div>`;
-        rowDiv.appendChild(cardDiv);
+        cardDiv.className = "card mb-2";
+
+        let imgThumbnailUrl = document.createElement("img");
+        imgThumbnailUrl.className = "card-img-top";
+        imgThumbnailUrl.src = `${response[i].thumbnailUrl}`;
+        imgThumbnailUrl.alt = "thumbnailUrl";
+
+        let cardBodyDiv = document.createElement("div");
+        cardBodyDiv.className = "card-body";
+
+        let h5Title = document.createElement("h5");
+        h5Title.className = "card-title";
+        h5Title.innerText = `${response[i].title}`;
+
+        let link = document.createElement("a");
+        link.href = "#";
+        link.className = "toggleButton";
+        link.id = response[i].id;
+        link.setAttribute("data-bs-toggle", "modal");
+        link.setAttribute("data-bs-target", "#exampleModal");
+        link.addEventListener("click", toggleOpen);
+
+        link.appendChild(h5Title);
+        cardBodyDiv.appendChild(link);
+        cardDiv.appendChild(imgThumbnailUrl);
+        cardDiv.appendChild(cardBodyDiv);
+        kapsayıcıDiv.appendChild(cardDiv);
+        rowDiv.appendChild(kapsayıcıDiv);
       }
     }
   };
