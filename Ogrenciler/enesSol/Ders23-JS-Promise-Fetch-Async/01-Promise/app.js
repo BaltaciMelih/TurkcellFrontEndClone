1c4650493d4c8ccd0bf6f19220b04888bbafb15d
enessol <94052771+enessol@users.noreply.github.com>
2022-11-28 22:09:08 +0300

    23rd lesson, Promise, Fetch, JSON
    23rd lesson, Promise, Fetch, JSON

---
+++ Ogrenciler/enesSol/Ders23-JS-Promise-Fetch-Async/01-Promise/app.js
@@ -0,0 +1,38 @@
+// function getData(data) {
+//   return new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//       if (typeof data === "string") {
+//         // Olumlu
+//         resolve("Data alındı...");
+//       } else {
+//         // Olumsuz
+//         reject(new Error("Sadece string girebilirsiniz!"));
+//       }
+//     }, 3000);
+//   });
+// }
+
+// getData("500")
+//   .then((response) => console.log("Gelen değer: " + response))
+//   .catch((error) => console.log(error));
+
+function x2(number) {
+  return new Promise((resolve, reject) => {
+    setTimeout(function () {
+      if (typeof number === "number") {
+        // Olumlu
+        resolve(number * 2);
+      } else {
+        reject(new Error("Lütfen sayı giriniz"));
+      }
+    }, 2000);
+  });
+}
+
+x2(4)
+  .then((response) => {
+    console.log(response);
+    return response * 2;
+  })
+  .then((res2) => console.log(res2))
+  .catch((err) => console.log(err));
---
+++ Ogrenciler/enesSol/Ders23-JS-Promise-Fetch-Async/02-Fetch/app.js
@@ -0,0 +1,30 @@
+// TEXT REQUEST
+
+// function getTextFile() {
+//   fetch("ornek.txt")
+//     .then((response) => response.text())
+//     .then((data) => console.log(data))
+//     .catch((err) => console.log("An error occured: ", err));
+// }
+
+// getTextFile();
+
+// JSON REQUEST
+
+// function getJsonFile() {
+//   fetch("users.json")
+//     .then((response) => response.json())
+//     .then((data) => console.log(data))
+//     .catch((err) => console.log("An error occured: ", err));
+// }
+
+// getJsonFile();
+
+// function getApi() {
+//   fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then((response) => response.json())
+//     .then((data) => console.log(data))
+//     .catch((err) => console.log("An error occured: ", err));
+// }
+
+// getApi();
