51d0a0b33be94f1561c2e3f92508482f1d18fadd
okanmaden <99764271+okanmaden@users.noreply.github.com>
2022-11-29 20:03:03 +0300

    Ders23

--- Ogrenciler/okanresitmaden/Ders_22/app.js
+++ Ogrenciler/okanresitmaden/Ders_22/app.js
@@ -1,6 +1,6 @@
-const deneme = document.getElementById("genel");
+const general = document.getElementById("genel");
 galery();
-const menemen = Number(prompt("Kaç Tane Fotoğraflık Bir Albüm İstersiniz?"));
+const photonumber = Number(prompt("Kaç Tane Fotoğraflık Bir Albüm İstersiniz?"));
 function galery(){
     const xhr = new XMLHttpRequest();
     xhr.open("GET","https://jsonplaceholder.typicode.com/photos");
@@ -8,8 +8,8 @@ function galery(){
         if(this.status == 200){
             const response = JSON.parse(this.responseText);
             response.forEach(function(object){
-                if(object.id <= menemen){
-                    deneme.innerHTML += `<div class="card col-lg-4 col-sm-12 " style="width: 18rem;">
+                if(object.id <= photonumber){
+                    general.innerHTML += `<div class="card col-md-4 col-12 " style="width: 18rem;">
                     <img src="${object.url}" class="card-img-top" id="img" alt="...">
                     <div class="card-body">
                     <h5 class="card-title" id="title">${object.title}</h5>
@@ -17,7 +17,6 @@ function galery(){
                 </div>`
                 }
             });
-
         }
     }
     xhr.send();
