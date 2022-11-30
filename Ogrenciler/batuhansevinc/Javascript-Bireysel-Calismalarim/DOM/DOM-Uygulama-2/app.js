3c129b836e0ff5ebd41189ecadc1dc43facc60da
BatuSevinc <batusvnc1@gmail.com>
2022-11-30 18:06:47 +0300

    ders23 & bireysel çalışmalarım

--- Ogrenciler/batuhansevinc/Javascript-Bireysel-Calismalarim/DOM/DOM-Uygulama-2/app.js
+++ Ogrenciler/batuhansevinc/Javascript-Bireysel-Calismalarim/DOM/DOM-Uygulama-2/app.js
@@ -1,16 +1,16 @@
 let sonuc;
-
 ul = document.getElementById("task-list");
+let count = ul.children.length;
+
 
 let li = `
 
 <li class="task list-group-item">
 <div class="form-check">
-<input type="checkbox" id="1" class="form-check-input">
-<label for="1" class="form-check-label">Görev 1</label>
+<input type="checkbox" id="${count+1}" class="form-check-input">
+<label for="${count+1}" class="form-check-label">Görev ${count+1}</label>
 </div>
 </li>
-
 `;
-
-console.log(sonuc);
+ul.insertAdjacentHTML("beforeend",li) // ul ' nin içerisine li etiketinden sonra bir li daha eklememizi sağladı. Web sitesinde görev 2 olarak gördüğün değeri aslında burada ekledik.
+console.log(li);
