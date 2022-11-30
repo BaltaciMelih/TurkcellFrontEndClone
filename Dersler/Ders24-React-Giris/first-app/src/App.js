3b4a4521f87355225b2c44b67795e5e0969715f3
Varol Maksutoglu <varolmaksutoglu@yandex.com>
2022-11-30 21:01:04 +0300

    react

--- Dersler/Ders24-React-Giris/first-app/src/App.js
+++ Dersler/Ders24-React-Giris/first-app/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Deneme from "./Deneme";
+import DenemeClass from "./DenemeClass";
 
 function App() {
   let name = "Varol";
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <h1>Hoşgeldin {name} </h1>
       <Deneme isim={name} />
+      <DenemeClass isim={name}></DenemeClass>
     </div>
   );
 }
