354305cc0764f56785ce39c72ef72e685ac04e47
Burak Nejat Songüler <nejat.980@hotmail.com>
2022-12-01 23:17:08 +0300

    boşluk belasını sildim

--- Ogrenciler/BurakNejatS/Odevler/odev3/script.js
+++ Ogrenciler/BurakNejatS/Odevler/odev3/script.js
@@ -9,7 +9,7 @@ function getBlog() {
 
         posts.innerHTML += `
         <div class="container col-md-4 col-sm-6 d-flex justify-content-center">
-        <div class="card" >
+        <div class="card">
                  
                  <img class="card-img-top" src=" ${json.url}">
 
@@ -21,11 +21,31 @@ function getBlog() {
                <div class="container">
                <p id="yazar">Yazar:${json.yazarAdı}</p>
                <p id="tarih" class="d-flex justify-content-end">Tarih:${json.dateItem}</p>
+
+
+               <div class="container">
+                 <p id="bolge" >Bölge:${json.bolge}</p>
+                 <a class="d-flex justify-content-end" href="#">Devamını Oku</a
+                 <div>
         
-               <a href="#" class="delete-item">
-                   <i class="fa fa-remove d-flex justify-content-end " onclick="deletePost(${json.id})">kaldır</i>
+              
+               <div class="container d-flex justify-content-center">
+
+               <div class="row">
+               <a href="#" class="text-decoration-none  ">
+                   <button class="btn btn-outline-danger  " onclick="deletePost(${json.id})">Kaldır</button>
                  </a>
-             </div> </div>
+                 </div>
+
+
+                 </div>
+                
+
+
+                 
+
+             </div> 
+             <br>
         `;
       }
     });
@@ -39,6 +59,7 @@ function createPost() {
     sehirAdı: document.getElementById("sehirAdı").value,
     geziYorum: document.getElementById("geziYorum").value,
     dateItem: document.getElementById("dateItem").value,
+    bolge:document.getElementById("bolge").value,
     url: document.getElementById("url").value,
   };
   fetch("http://localhost:3000/blogs", {
@@ -53,9 +74,8 @@ function createPost() {
 }
 
 function deletePost(id) {
-  fetch("http://localhost:3000/blogs" + id, {
+  fetch("http://localhost:3000/blogs" + "/" + id, {
     method: "DELETE",
-    headers: { "Content-Type": "application/json;charset=UTF-8" ,}
   })
     .then((response) => console.log(response))
     .then((json) => {
@@ -64,4 +84,27 @@ function deletePost(id) {
     .catch((error) => console.log(error));
 }
 
-deletePost(id);
+const filter = document.getElementById("filter");
+
+
+function filterPost() {
+  // const filterValue = e.targer.value.toLowerCase();
+  const listPost = document.querySelector("input");
+  
+  listPost.addEventListener("keyup",function(){
+    let filterValue=this.value;
+    
+    let li =document.querySelectorAll(".card");
+
+    for(let i= 0; i< li.length; i++){
+      // console.log(li[i].innerHTML);
+      if(li[i].innerHTML.indexOf(filterValue)>-1){
+        li[i].style.display="";
+      }else{
+        li[i].style.display="none";
+      }
+    }
+
+  });
+}
+filterPost();
