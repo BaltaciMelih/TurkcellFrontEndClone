2e128a5785cb3fa03fbcd549a969729edd6cf663
Burak Nejat Songüler <nejat.980@hotmail.com>
2022-12-02 20:19:59 +0300

    Ders24 ve Odev3 update

--- Ogrenciler/BurakNejatS/Ders24-React/first-app/src/deneme.jsx
+++ Ogrenciler/BurakNejatS/Ders24-React/first-app/src/deneme.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import State, { useState } from 'react';
+import React from 'react';
 
-export default function deneme(props) {
+
+
+export default function Deneme() {
+  const [count,setCount]=useState(0);
   return (
-    <div>bu bir componenttir {props.isim}</div>
+    <div><h1>sayaç</h1>
+    <p>{count} </p>
+      <button onClick={() => setCount(count + 1)}>
+        Arttır
+      </button>
+      <button onClick={() => setCount(count - 1)}>
+        Azalt
+      </button>
+      <button onClick={() => setCount(count * 0)}>
+        sıfırla
+      </button>
+    
+    
+    </div>
   )
 }
+
+
+// export const Button = (props) => {
+//   return (
+//     <button onClick={() =>
+//       props.sign == "+" ? props.updateCount(1) : props.updateCount(-1)} >
+//       {props.sign}
+//     </button>
+//   );
+// }
