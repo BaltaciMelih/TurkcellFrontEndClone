5795b3a1d98fbb35abd00a2fe0733e4b1116d5fb
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-12-03 23:25:19 +0300

    Axios issue handled, React Router general logic understood.

--- Ogrenciler/halilEnesOzdemir/Bireysel_Calismalar/React/Day06/fetchAxios/src/App.jsx
+++ Ogrenciler/halilEnesOzdemir/Bireysel_Calismalar/React/Day06/fetchAxios/src/App.jsx
@@ -1,12 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
 import AddUser from './fetchSample/AddCategory';
+import AddCategoryWithAxios from './fetchSample/AddCategoryWithAxios';
 import CategoryPost from './fetchSample/CategoryPost';
-import InputCategoryPost from './fetchSample/InputCategoryPost';
+// import InputCategoryPost from './fetchSample/InputCategoryPost';
 import UserList from './fetchSample/UserList';
+import About from './routeSample/About';
+import Contact from './routeSample/Contact';
+import Footer from './routeSample/Footer';
+import Header from './routeSample/Header';
+import Homepage from './routeSample/Homepage';
 
 function App() {
   return (
     <>
-      <InputCategoryPost></InputCategoryPost>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Homepage />}></Route>
+        <Route path='/hakkimizda' element={<About />}></Route>
+        <Route path='/iletisim' element={<Contact />}></Route>
+      </Routes>
+      <Footer></Footer>
     </>
   );
 }
