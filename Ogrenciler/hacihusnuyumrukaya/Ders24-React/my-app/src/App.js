9faa563cdfa5d7a644259bd918d1d0e7c4140ac3
HaciHusnuYumrukaya26 <114759482+HaciHusnuYumrukaya26@users.noreply.github.com>
2022-12-03 09:44:26 +0300

    Ders24-state

--- Ogrenciler/hacihusnuyumrukaya/Ders24-React/my-app/src/App.js
+++ Ogrenciler/hacihusnuyumrukaya/Ders24-React/my-app/src/App.js
@@ -1,14 +1,17 @@
-
-import './App.css';
-import Deneme from './Deneme';
-import DenemeClass from './DenemeClass';
+import "./App.css";
+import Deneme from "./Deneme";
+import DenemeClass from "./DenemeClass";
+import StateComponent from "./StateComponent";
+import StateComponentUyg from "./StateComponentUyg";
 function App() {
-  let name="Hacı";
+  let name = "Hacı";
   return (
     <div className="App">
-      <h1>Hoşgeldin {name}</h1>
-      <Deneme isim={name}/>
-      <DenemeClass isim={name}/>
+      <h1>Hoşgeldin {name} </h1>
+      <Deneme isim={name} id={5} />
+      <DenemeClass isim={name}></DenemeClass>
+      <StateComponent />
+      <StateComponentUyg />
     </div>
   );
 }
