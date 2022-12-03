0c90142b261a95e64993860c320098997e0c000f
Tugcenrgn <tugcenurergen9@gmail.com>
2022-12-03 14:02:27 +0300

    Odev3

--- Ogrenciler/tugcenurergen/Odevler/Ecommerce-with-json/src/script.js
+++ Ogrenciler/tugcenurergen/Odevler/Ecommerce-with-json/src/script.js
@@ -49,9 +49,9 @@ class Request {
         headers: {
           "Content-type": "application/json; charset=UTF-8",
         },
-      })
-        .then((response) => response.json())
-        .then((data) => resolve(data))
+       })
+      //   .then((response) => response.json())
+      //   .then((data) => resolve(data))
         .catch((err) => reject(err));
     });
   }
@@ -67,7 +67,13 @@ class Request {
   }
 }
 
+
 const request = new Request("http://localhost:3000/product");
+
+
+//events
+cardContainer.addEventListener("click", deleteProduct);
+cardContainer.addEventListener("click", editProduct);
 
 request
   .get()
@@ -100,132 +106,107 @@ function deleteProduct(e) {
   }
 }
 
-// function editProduct(e){
-//   e.preventDefault();
-//   const htmlElement = e.target;
-//   if (htmlElement.classList.contains("edit-btn")){
-//     const editName = productName
-//     const editDescription = productDescription
-//     const editUrl1 = productUrl1
-//     const editUrl2 = productUrl2
-//     const editCard = htmlElement.parentElement.parentElement;
-//     const editId = htmlElement.id;
-//     const editDate = Date();
 
-//     request.get()
-//     .then((data) => {
-//       data.forEach((element) => {
-//         console.log("foreach");
-//         if (element.id == editId) {
-//           console.log("if")
-//           editName.value = element.name;
-//           editDescription.value = element.description;
-//           editUrl1.value = element.url1;
-//           editUrl2.value = element.url2;
-//         }
-//       });
-//     }
-//     )
-//     .catch((err) => console.log(err));
+//verileri forma çekemiyorum ama edit butonuna basıp forma yeni bilgiler girildiğinde editleme yapılıyor.
+function editProduct(e){
+  e.preventDefault();
+  const htmlElement = e.target;
+  if (htmlElement.classList.contains("edit-btn")){
+    const editName = productName
+    const editDescription = productDescription
+    const editUrl1 = productUrl1
+    const editUrl2 = productUrl2
+    const editCard = htmlElement.parentElement.parentElement;
+    const editId = htmlElement.id;
+    const editDate = Date();
 
-//     form.addEventListener("submit", function(e) {
-//       request.put(editId, {
-//         name: editName.value,
-//         description: editDescription.value,
-//         url1: editUrl1.value,
-//         url2: editUrl2.value
-//       })
-//       .then((data) => {
-//         editCard.innerHTML = `
-//         <div
-//               class="card w-100 "
-//               id= "${editId}"
-//               >
-//               <div class="img-container">
-//                 <img
-//                 src="${editUrl1}"
-//                 class="card-img-top"
-//                 alt="..." />
-//                 <div class="overlay">
-//                   <img class="overlay-img" src="${url2}" alt="">
-//                 </div>
-//               </div>
-//               <div class="card-body">
-//                 <h3 class="card-product-header">${editName}</h3>
-//                 <p class="card-text">
-//                   ${editDescription}
-//                 </p>
-//                 <span class="float-end">Tarih: <br/> ${editDate}</span>
+    // request.get()
+    // .then((data) => {
+    //   data.forEach((element) => {
+    //     console.log("foreach");
+    //     if (element.id == editId) {
+    //       console.log("if")
+    //       editName.value = element.name;
+    //       editDescription.value = element.description;
+    //       editUrl1.value = element.url1;
+    //       editUrl2.value = element.url2;
+    //     }
+    //   });
+    // }
+    // )
+    // .catch((err) => console.log(err));
 
-//                 <button id= "${editId}" type="button" class="btn btn-dark delete-btn">Delete</button>
-//                 <button id= "${editId}" type="button" class="btn btn-light edit-btn">Edit</button>
+    form.addEventListener("submit", function(e) {
+      request.put(editId, {
+        name: editName.value,
+        description: editDescription.value,
+        url1: editUrl1.value,
+        url2: editUrl2.value
+      })
+      .then((data) => {
+        editCard.innerHTML = `
+        <div
+              class="card w-100 "
+              id= "${data.editId}"
+              >
+              <div class="img-container">
+                <img
+                src="${editUrl1}"
+                class="card-img-top"
+                alt="..." />
+                <div class="overlay">
+                  <img class="overlay-img" src="${url2}" alt="">
+                </div>
+              </div>
+              <div class="card-body">
+                <h3 class="card-product-header">${data.editName}</h3>
+                <p class="card-text">
+                  ${editDescription}
+                </p>
+                <span class="float-end">Tarih: <br/> ${editDate}</span>
 
-//               </div>
-//             </div>
-//         `;
-//       })
-//     })
+                <button id= "${editId}" type="button" class="btn btn-dark delete-btn">Delete</button>
+                <button id= "${editId}" type="button" class="btn btn-light edit-btn">Edit</button>
 
-//   }
-// }
+              </div>
+            </div>
+        `;
+      })
+      
+        request
+          .delete(Number(editId)) //id stirng olarak alındığından numbera çevirdim.
+          .then((msg) => console.log(msg))
+          .catch((err) => console.log(err));
+    })
 
-//events
-cardContainer.addEventListener("click", deleteProduct);
-//cardContainer.addEventListener("click", editProduct);
+  }
+}
 
-// {
-//   e.preventDefault();
-//   const htmlElement = e.target;
-//   if (htmlElement.classList.contains("delete-btn")) {
-//     const deletingId = htmlElement.parentElement.parentElement.id;
-//     request
-//       .delete(Number(deletingId)) //id stirng olarak alındığından numbera çevirdim.
-//       .then((msg) => console.log(msg))
-//       .catch((err) => console.log(err));
-//   }
 
-//   if (htmlElement.parentElement.parentElement.classList.contains("card")) {
-//     const forwardingId = htmlElement.parentElement.parentElement.id;
-//     //ürüne tıklandığında o ürüne ait sayfaya yönlendirir
-
-//     window.setTimeout(function () {
-//       location.href = "./product.html";
-//     }, 450);
-
-//     //ekrana bilgileri yazdıralım
-//     productsArr.map((element) => {
-//       if (element.id == forwardingId) {
-//         const newProduct= {
-//           productName: element.name,
-//           productDescription: element.description,
-//           productPageUrl1: element.url1,
-//           productPageUrl2: element.url2
-
-//         };
-//         // const productPageName = element.name;
-//         // const productPageDescription = element.description;
-//         // const productPageUrl1 = element.url1;
-//         // // const productPageUrl2 = element.url2;
-
-//         // console.log(loadProductPage(newProduct)+ "temp");
-//       }
-//       return
-//     });
-//   }
-// });
 
 //form submit edildiğinde yeni ürün eklensin
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   const newPost = {
     id: Math.floor(Math.random() * 1000),
-    name: `${document.getElementById("name").value}`,
-    description: `${document.getElementById("description").value}`,
-    url1: `${document.getElementById("url1").value}`,
-    url2: `${document.getElementById("url2").value}`,
+    name: `${document.getElementById("name").value.trim()}`,
+    description: `${document.getElementById("description").value.trim()}`,
+    url1: `${document.getElementById("url1").value.trim()}`,
+    url2: `${document.getElementById("url2").value.trim()}`,
     date: date(), ///
   };
-  request.post(newPost).then((data) => console.log(data));
+  if (
+    newPost.name === "" ||
+    newPost.description === "" ||
+    newPost.url1 === "" ||
+    newPost.url2 === ""
+  ) {
+    alert("Please enter all required");
+  } else {
+    request
+    .post(newPost)
+    .then((data) => console.log(data));
+  }
 });
 
 //ekrana json verilerini bastırma
@@ -346,56 +327,3 @@ function createCard({ id, name, description, url1, url2, date }) {
 
   return cardDiv;
 }
-
-// function loadProductPage(
-//   {productPageName,
-//   productPageDescription,
-//   productPageUrl1,
-//   productPageUrl2}
-// ) {
-//     console.log(productPageName);
-//   let productPageContainer = document.createElement("div");
-//   productPageContainer.classList.add("container");
-//   productPageContainer.innerHTML = `
-//     <div class="row">
-//         <div class="col product-img-col">
-//           <img
-//             class="product-img"
-//             src="https://res.cloudinary.com/castlery/image/private/w_900,f_auto,q_auto/b_rgb:F3F3F3,c_fit/v1645672292/crusader/variants/50440749-AM4001/Madison-Right-Chaise-Sectional-Sofa-Bisque-Angle.jpg"
-//             alt="" />
-//         </div>
-//       </div>
-
-//       <div class="row">
-//         <div class="col product-header-div">
-//           <h3>Product</h3>
-//         </div>
-//       </div>
-//       <div class="row">
-//         <div class="col product-text-div">
-//           <p class="product-text">
-//             Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
-//             labore modi placeat magnam, corporis consectetur?
-//           </p>
-//         </div>
-//       </div>
-//       <div class="row">
-//         <div class="col product-header-div">
-//           <h4>Colors</h4>
-//         </div>
-//       </div>
-
-//       <div class="row">
-//         <div class="col product-text-div">
-//           <div class="color1"></div>
-//           <div class="color2"></div>
-//           <div class="color3"></div>
-//           <div class="color4"></div>
-//           <div class="color5"></div>
-//         </div>
-//       </div>
-//     `;
-
-//     console.log(productPageContainer);
-//     container.append(productPageContainer);
-// }
