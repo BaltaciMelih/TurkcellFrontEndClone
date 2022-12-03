1afb1911f64c9b95a9fcd1fc068789bf321f993a
Tugcenrgn <tugcenurergen9@gmail.com>
2022-12-03 13:09:01 +0300

    update

--- Ogrenciler/tugcenurergen/Hafta-10/Ders-2/FirstApp/my-app/src/Template.jsx
+++ Ogrenciler/tugcenurergen/Hafta-10/Ders-2/FirstApp/my-app/src/Template.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-
-export default function Template(props){
+import PropTypes from 'prop-types'
+export default function Template({isim, baslik, id}){
     return(
         <div>
-            <h2>template component {props.name}</h2>
+            <h2>template component {isim} {baslik}{id}</h2>
         </div>
     )
 } 
+
+Template.propTypes = {
+    isim: PropTypes.string,
+    baslik: PropTypes.string,
+    id: PropTypes.number
+}
--- Ogrenciler/tugcenurergen/Odevler/Ecommerce-with-json/src/script.js
+++ Ogrenciler/tugcenurergen/Odevler/Ecommerce-with-json/src/script.js
@@ -5,8 +5,6 @@ let productUrl1 = document.querySelector("#url1");
 let productUrl2 = document.querySelector("#url2");
 let form = document.querySelector("#form");
 let container = document.querySelector("#productContainer");
-// let cardContainer = document.querySelector(".card-container");
-
 class Request {
   constructor(url) {
     this.url = url;
@@ -27,6 +25,7 @@ class Request {
         })
         .catch((err) => reject(err));
     });
+    return response;
   }
 
   async post(data) {
@@ -69,12 +68,14 @@ class Request {
 }
 
 const request = new Request("http://localhost:3000/product");
+
 request
   .get()
   .then((data) => console.log(data))
   .catch((err) => console.log(err));
 
-console.log(request.products);
+let productsArr = request.products;
+console.log(productsArr);
 
 function date() {
   var date = new Date();
@@ -87,102 +88,131 @@ function date() {
   return date;
 }
 
-//events
-cardContainer.addEventListener("click", function (e) {
-  console.log(e.target);
+function deleteProduct(e) {
+  e.preventDefault();
   const htmlElement = e.target;
   if (htmlElement.classList.contains("delete-btn")) {
-    console.log(htmlElement.id);
     const deletingId = htmlElement.parentElement.parentElement.id;
-    console.log(deletingId);
     request
       .delete(Number(deletingId)) //id stirng olarak alındığından numbera çevirdim.
       .then((msg) => console.log(msg))
       .catch((err) => console.log(err));
   }
-
-  if (htmlElement.parentElement.parentElement.classList.contains("card")) {
-    console.log("in card");
-    const forwardingId = htmlElement.parentElement.parentElement.id;
-    console.log(forwardingId);
-    //ürüne tıklandığında o ürüne ait sayfaya yönlendirir
-
-    window.setTimeout(function () {
-      location.href = "./product.html";
-    }, 450);
-
-    //ekrana bilgileri yazdıralım
-    request.products.map((element) => {
-      if (element.id == forwardingId) {
-        const productPageName = element.name;
-        const productPageDescription = element.description;
-        const productPageUrl1 = element.url1;
-        const productPageUrl2 = element.url2;
-        loadProductPage(
-          productPageName,
-          productPageDescription,
-          productPageUrl1,
-          productPageUrl2
-        )
-      }
-    });
-  }
-});
-
-
-
-function loadProductPage(
-  productPageName,
-  productPageDescription,
-  productPageUrl1,
-  productPageUrl2
-) {
-    console.log(productPageName);
-  let productPageContainer = document.createElement("div");
-  productPageContainer.classList.add(".container");
-  productPageContainer.innerHTML = `
-    <div class="row">
-        <div class="col product-img-col">
-          <img
-            class="product-img"
-            src="https://res.cloudinary.com/castlery/image/private/w_900,f_auto,q_auto/b_rgb:F3F3F3,c_fit/v1645672292/crusader/variants/50440749-AM4001/Madison-Right-Chaise-Sectional-Sofa-Bisque-Angle.jpg"
-            alt="" />
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col product-header-div">
-          <h3>Product</h3>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col product-text-div">
-          <p class="product-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
-            labore modi placeat magnam, corporis consectetur?
-          </p>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col product-header-div">
-          <h4>Colors</h4>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col product-text-div">
-          <div class="color1"></div>
-          <div class="color2"></div>
-          <div class="color3"></div>
-          <div class="color4"></div>
-          <div class="color5"></div>
-        </div>
-      </div>
-    `;
-
-    console.log(productPageContainer);
-    container.append(productPageContainer);
 }
+
+// function editProduct(e){
+//   e.preventDefault();
+//   const htmlElement = e.target;
+//   if (htmlElement.classList.contains("edit-btn")){
+//     const editName = productName
+//     const editDescription = productDescription
+//     const editUrl1 = productUrl1
+//     const editUrl2 = productUrl2
+//     const editCard = htmlElement.parentElement.parentElement;
+//     const editId = htmlElement.id;
+//     const editDate = Date();
+
+//     request.get()
+//     .then((data) => {
+//       data.forEach((element) => {
+//         console.log("foreach");
+//         if (element.id == editId) {
+//           console.log("if")
+//           editName.value = element.name;
+//           editDescription.value = element.description;
+//           editUrl1.value = element.url1;
+//           editUrl2.value = element.url2;
+//         }
+//       });
+//     }
+//     )
+//     .catch((err) => console.log(err));
+
+//     form.addEventListener("submit", function(e) {
+//       request.put(editId, {
+//         name: editName.value,
+//         description: editDescription.value,
+//         url1: editUrl1.value,
+//         url2: editUrl2.value
+//       })
+//       .then((data) => {
+//         editCard.innerHTML = `
+//         <div
+//               class="card w-100 "
+//               id= "${editId}"
+//               >
+//               <div class="img-container">
+//                 <img
+//                 src="${editUrl1}"
+//                 class="card-img-top"
+//                 alt="..." />
+//                 <div class="overlay">
+//                   <img class="overlay-img" src="${url2}" alt="">
+//                 </div>
+//               </div>
+//               <div class="card-body">
+//                 <h3 class="card-product-header">${editName}</h3>
+//                 <p class="card-text">
+//                   ${editDescription}
+//                 </p>
+//                 <span class="float-end">Tarih: <br/> ${editDate}</span>
+
+//                 <button id= "${editId}" type="button" class="btn btn-dark delete-btn">Delete</button>
+//                 <button id= "${editId}" type="button" class="btn btn-light edit-btn">Edit</button>
+
+//               </div>
+//             </div>
+//         `;
+//       })
+//     })
+
+//   }
+// }
+
+//events
+cardContainer.addEventListener("click", deleteProduct);
+//cardContainer.addEventListener("click", editProduct);
+
+// {
+//   e.preventDefault();
+//   const htmlElement = e.target;
+//   if (htmlElement.classList.contains("delete-btn")) {
+//     const deletingId = htmlElement.parentElement.parentElement.id;
+//     request
+//       .delete(Number(deletingId)) //id stirng olarak alındığından numbera çevirdim.
+//       .then((msg) => console.log(msg))
+//       .catch((err) => console.log(err));
+//   }
+
+//   if (htmlElement.parentElement.parentElement.classList.contains("card")) {
+//     const forwardingId = htmlElement.parentElement.parentElement.id;
+//     //ürüne tıklandığında o ürüne ait sayfaya yönlendirir
+
+//     window.setTimeout(function () {
+//       location.href = "./product.html";
+//     }, 450);
+
+//     //ekrana bilgileri yazdıralım
+//     productsArr.map((element) => {
+//       if (element.id == forwardingId) {
+//         const newProduct= {
+//           productName: element.name,
+//           productDescription: element.description,
+//           productPageUrl1: element.url1,
+//           productPageUrl2: element.url2
+
+//         };
+//         // const productPageName = element.name;
+//         // const productPageDescription = element.description;
+//         // const productPageUrl1 = element.url1;
+//         // // const productPageUrl2 = element.url2;
+
+//         // console.log(loadProductPage(newProduct)+ "temp");
+//       }
+//       return
+//     });
+//   }
+// });
 
 //form submit edildiğinde yeni ürün eklensin
 form.addEventListener("submit", function (e) {
@@ -224,16 +254,148 @@ function createCard({ id, name, description, url1, url2, date }) {
                 </div>
               </div>
               <div class="card-body">
-                <h3 class="card-product-header">${name}</h3>
-                <p class="card-text">
-                  ${description}
-                </p>
-                <span class="float-end">${date}</span>
+                <h3 class="card-product-header">${name}
+                <button
+                  type="button"
+                  class="btn btn-light"
+                  data-bs-toggle="modal"
+                  data-bs-target="#exampleModal">
+                  Details
+                </button>
+                </h3>
+              
+                <!-- Modal -->
                 
-                <button id= "${id}" type="button" class="btn btn-dark delete-btn">Dark</button>
+                <div
+                  class="modal fade"
+                  id="exampleModal"
+                  tabindex="-1"
+                  aria-labelledby="exampleModalLabel"
+                  aria-hidden="true">
+                  <div class="modal-dialog modal-md modal-dialog-centered">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">
+                          Product
+                        </h1>
+                        <button
+                          type="button"
+                          class="btn-close"
+                          data-bs-dismiss="modal"
+                          aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                         <div id="productContainer" class="p-container">
+                            <div class="row">
+                              <div class="col product-img-col">
+                                <img
+                                  class="product-img"
+                                  src="${url1}"
+                                  alt="" />
+                              </div>
+                            </div>
+
+                            <div class="row">
+                              <div class="col product-header-div">
+                                <h3>${name}</h3>
+                              </div>
+                            </div>
+                            <div class="row">
+                              <div class="col product-text-div">
+                                <p class="product-text">
+                                  ${description}
+                                </p>
+                              </div>
+                            </div>
+                            <div class="row">
+                              <div class="col product-header-div">
+                                <h4>Colors</h4>
+                              </div>
+                            </div>
+
+                            <div class="row">
+                              <div class="col product-text-div">
+                                <div class="color1"></div>
+                                <div class="color2"></div>
+                                <div class="color3"></div>
+                                <div class="color4"></div>
+                                <div class="color5"></div>
+                              </div>
+                            </div>
+
+                            <div class="row">
+                              <div class="col product-text-div">
+                                <button class="btn btn-warning ">Add to Chart</button>
+                              </div>
+                            </div>
+                          </div>
+                      </div>
+                      
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div>
+              
+                <span class="float-end">Tarih: <br/> ${date}</span>
+                <button id= "${id}" type="button" class="btn btn-dark delete-btn">Delete</button>
+                <button id= "${id}" type="button" class="btn btn-light edit-btn">Edit</button></div>
                 
               </div>
-            </div>`;
+            `;
 
   return cardDiv;
 }
+
+// function loadProductPage(
+//   {productPageName,
+//   productPageDescription,
+//   productPageUrl1,
+//   productPageUrl2}
+// ) {
+//     console.log(productPageName);
+//   let productPageContainer = document.createElement("div");
+//   productPageContainer.classList.add("container");
+//   productPageContainer.innerHTML = `
+//     <div class="row">
+//         <div class="col product-img-col">
+//           <img
+//             class="product-img"
+//             src="https://res.cloudinary.com/castlery/image/private/w_900,f_auto,q_auto/b_rgb:F3F3F3,c_fit/v1645672292/crusader/variants/50440749-AM4001/Madison-Right-Chaise-Sectional-Sofa-Bisque-Angle.jpg"
+//             alt="" />
+//         </div>
+//       </div>
+
+//       <div class="row">
+//         <div class="col product-header-div">
+//           <h3>Product</h3>
+//         </div>
+//       </div>
+//       <div class="row">
+//         <div class="col product-text-div">
+//           <p class="product-text">
+//             Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+//             labore modi placeat magnam, corporis consectetur?
+//           </p>
+//         </div>
+//       </div>
+//       <div class="row">
+//         <div class="col product-header-div">
+//           <h4>Colors</h4>
+//         </div>
+//       </div>
+
+//       <div class="row">
+//         <div class="col product-text-div">
+//           <div class="color1"></div>
+//           <div class="color2"></div>
+//           <div class="color3"></div>
+//           <div class="color4"></div>
+//           <div class="color5"></div>
+//         </div>
+//       </div>
+//     `;
+
+//     console.log(productPageContainer);
+//     container.append(productPageContainer);
+// }
