16201731ee0e30663405eca11cd4e4c01b2ba8a6
merkaya1 <110626800+merkaya1@users.noreply.github.com>
2022-12-06 01:01:11 +0300

    ecommerce Signup and Login Components
    SignUp ve Login componentlerindeki inputlara onchance fonksiyonu yazıldı ve parametrelerini useState parametrelerinden aldı,

--- Ogrenciler/mustafaErkaya/Odevler/Final-Ecommerce-App/ecommerce-app/src/Components/Login.jsx
+++ Ogrenciler/mustafaErkaya/Odevler/Final-Ecommerce-App/ecommerce-app/src/Components/Login.jsx
@@ -1,25 +1,39 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Link} from 'react-router-dom'
 
 export const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [errorMsg, setErrorMsg]=useState('');
+    const [successMsg, setSuccessMsg]=useState('');
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        // console.log( email, password );
+    }
+
   return (
     <div className='container ' >
     <div className='row '>
         <div className='col-6 mx-auto'>
             <h1>Login</h1>
             <hr />
-            <form className='form-group' autoComplete='off'>
+            <form className='form-group ' autoComplete='off' onSubmit={handleLogin}>
                 
 
                 <label >Email</label>
-                <input type="email" className='form-control mb-3' required />
+                <input type="email" className='form-control mb-3' required  
+                onChange={(e)=>setEmail(e.target.value)} value={email}/>
 
                 <label >Password</label>
-                <input type="password" className='form-control mb-3' required />
+                <input type="password" className='form-control mb-3' required 
+                onChange={(e)=>setPassword(e.target.value)} value={password}/>
 
                 
 
-                <div className='d-flex justify-content-between align-items-center mt-4'>
+                <div className='d-flex flex-column flex-md-row  justify-content-between align-items-center mt-4'>
                 <span>Don't have an account SignUp
                 <Link to="/signup" className='link'> Here</Link></span>
                 <button type='submit' className='btn btn-success '>LOGIN</button>
--- Ogrenciler/mustafaErkaya/Odevler/Final-Ecommerce-App/ecommerce-app/src/Components/Signup.jsx
+++ Ogrenciler/mustafaErkaya/Odevler/Final-Ecommerce-App/ecommerce-app/src/Components/Signup.jsx
@@ -1,30 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 
 export const Signup = () => {
+
+    const [fullName, setFullname] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [phone, setPhone] = useState('');
+
+    const [errorMsg, setErrorMsg]=useState('');
+    const [successMsg, setSuccessMsg]=useState('');
+
+    const handleSignup =(e) => {
+        e.preventDefault();
+        // console.log(fullName, email, password, phone);
+    }
+
   return (
     <div className='container ' >
         <div className='row'>
             <div className='col-6 mx-auto'>
                 <h1>Sign Up</h1>
                 <hr />
-                <form className='form-group' autoComplete='off'>
+                <form className='form-group ' autoComplete='off' onSubmit={handleSignup}>
                     <label >Full Name</label>
-                    <input type="text" className='form-control mb-3' required />
+                    <input type="text" className='form-control mb-3' required 
+                     onChange={(e)=>setFullname(e.target.value)} value={fullName}/>
 
                     <label >Email</label>
-                    <input type="email" className='form-control mb-3' required />
+                    <input type="email" className='form-control mb-3' required   
+                    onChange={(e)=>setEmail(e.target.value)} value={email}/>
 
                     <label >Password</label>
-                    <input type="password" className='form-control mb-3' required />
+                    <input type="password" className='form-control mb-3' required   
+                    onChange={(e)=>setPassword(e.target.value)} value={password}/>
 
                     <label >Phone</label>
-                    <input type="number" className='form-control mb-3' required />
+                    <input type="number" className='form-control mb-3' required   
+                    onChange={(e)=>setPhone(e.target.value)} value={phone}/>
 
-                    <div className='d-flex justify-content-between align-items-center mt-4'>
+                    <div className='d-flex flex-column flex-md-row justify-content-between align-items-center mt-4'>
                     <span>Already have an account Login
                     <Link to="/login" className='link'> Here</Link></span>
-                    <button type='submit' className='btn btn-success '>SING UP</button>
+                    <button type='submit' className='btn btn-success btn-md'>SING UP</button>
                     </div>
                 </form>
             </div>
