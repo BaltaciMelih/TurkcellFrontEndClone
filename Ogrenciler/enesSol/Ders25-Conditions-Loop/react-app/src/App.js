770a6073bab3362fdb328a0301b0d706dc8819a2
enessol <94052771+enessol@users.noreply.github.com>
2022-12-06 00:32:56 +0300

    25th lesson, React Conditions
    25th lesson, React Conditions

---
+++ Ogrenciler/enesSol/Ders25-Conditions-Loop/react-app/src/App.js
@@ -0,0 +1,47 @@
+import "./App.css";
+import ListItem from "./ListItem";
+import { useState, useEffect } from "react";
+
+function App() {
+  const students = ["Varol", "Halil", "Şafak", "Mustafa", "EnesSöl"];
+  const itemList = [
+    <li key={1}>Deneme 1</li>,
+    <li key={2}>Deneme 2</li>,
+    <li key={3}>Deneme 3</li>,
+    <li key={4}>Deneme 4</li>,
+  ];
+
+  return (
+    <div className="App">
+      <form>
+        <input type="search" />
+      </form>
+      <ul>
+        {students.map((student, index) => (
+          <ListItem key={index} student={student}></ListItem>
+        ))}
+      </ul>
+      <ul>{itemList}</ul>
+      <ul>
+        {students.map((item, index) => {
+          return (
+            <li key={index}>
+              {item.length > 5 ? item.toUpperCase() : item.toLowerCase()}
+            </li>
+          );
+        })}
+      </ul>
+      <ul>
+        {students.map((item, index) => {
+          if (item.includes("Varol")) {
+            return null;
+          } else {
+            return item;
+          }
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
