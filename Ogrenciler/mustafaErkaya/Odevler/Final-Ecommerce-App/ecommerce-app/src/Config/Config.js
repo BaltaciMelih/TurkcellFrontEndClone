773c6b6ab38bcf81bca19a95d2f9439a269b4236
merkaya1 <110626800+merkaya1@users.noreply.github.com>
2022-12-06 01:32:00 +0300

    e-commerce Firebase
    Firebase projeye indirildi
    -Config.js dosyasına config edildi
    -auth, firestore, storage firebaseden Config.js'e impor edildi.
    -Değişkenler oluşturuldu ve bu değişkenler diğer sayfalarda veri çekme ve kaydetme işlerinde kullanılacak

---
+++ Ogrenciler/mustafaErkaya/Odevler/Final-Ecommerce-App/ecommerce-app/src/Config/Config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase'
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/storage'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCYCs1tV6gDC4TWiM7A8lbp33Ep0zCoXK0",
+    authDomain: "ecommerce-app-mustafa.firebaseapp.com",
+    projectId: "ecommerce-app-mustafa",
+    storageBucket: "ecommerce-app-mustafa.appspot.com",
+    messagingSenderId: "353170437669",
+    appId: "1:353170437669:web:a6e300a55c55f9f5ea31cc"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+  const auth = firebase.auth();
+  const fs = firebase.firestore();
+  const storage = firebase.storage();
+
+    export {auth,fs,storage}
