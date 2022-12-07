a3ebf9fca2fa263f9fca7b40567c98ed0a949c6c
Mertcan Moran <mertcanmmoran@gmail.com>
2022-12-07 21:34:10 +0300

    Ders26-Styled components

--- Ogrenciler/mertcanMoran/Ders26/styledcomponents/src/App.js
+++ Ogrenciler/mertcanMoran/Ders26/styledcomponents/src/App.js
@@ -1,26 +1,32 @@
 import "./App.css";
 import { useState } from "react";
+import styled from "styled-components";
+import styles from "./CustomStyle.module.css";
 
 function App() {
   const [data, setData] = useState([
     {
       id: 1,
       name: "mertcan",
+      status: "error",
       grade: 45,
     },
     {
       id: 2,
       name: "burak",
+      status: "primary",
       grade: 30,
     },
     {
       id: 3,
       name: "berat",
+      status: "success",
       grade: 90,
     },
     {
       id: 4,
       name: "fatma",
+      status: "success",
       grade: 70,
     },
   ]);
@@ -44,31 +50,68 @@ function App() {
       return "aa";
     }
   };
+
+  const Button = styled.button`
+    color: #5c01a1;
+    font-size: 20px;
+    border: none;
+    outline: none;
+    padding: 8px 16px;
+  `;
+  const PrimaryButton = styled(Button)`
+    color: #fff;
+    background-color: #1901a1;
+    font-size: ${(props) => (props.size ? `${props.size}px` : "16px")};
+  `;
   return (
     <div className="App">
-      {sorted.map((item, index) => {
+      {data.map((item, index) => {
         return (
           <div key={index} className={`gradeStatus ${getGrades(item)}`}>
             <p>
-              Sınav sonucuna göre harf notu: {getGrades(item)} - Name:
-              {item.name} - Grade: {item.grade}
+              Sonuca göre harf notu: {getGrades(item)} - Grade: {item.grade}
             </p>
           </div>
         );
       })}
-      <br />
-      {sorted2.map((item, index) => {
-        return (
-          <div key={index} className={`gradeStatus ${getGrades(item)}`}>
-            <p>
-              Sınav sonucuna göre harf notu: {getGrades(item)} - Name:
-              {item.name} - Grade: {item.grade}
-            </p>
-          </div>
-        );
-      })}
+      <hr />
+      <button className="customButton">Custom Button</button>
+      <Button>Styled Button</Button>
+      <PrimaryButton size="30">Primary Button</PrimaryButton>
+      <hr />
+      <div className={styles.moduleCss}>Merhaba</div>
+      <button className={styles.customButton}>Selam</button>
+      <hr />
+      {data.map((item) => (
+        <div className={styles[item.status]}>{item.name}</div>
+      ))}
     </div>
   );
+  // return (
+  //   <div className="App">
+  //     {sorted.map((item, index) => {
+  //       return (
+  //         <div key={index} className={`gradeStatus ${getGrades(item)}`}>
+  //           <p>
+  //             Sınav sonucuna göre harf notu: {getGrades(item)} - Name:
+  //             {item.name} - Grade: {item.grade}
+  //           </p>
+  //         </div>
+  //       );
+  //     })}
+  //     <br />
+  //     {sorted2.map((item, index) => {
+  //       return (
+  //         <div key={index} className={`gradeStatus ${getGrades(item)}`}>
+  //           <p>
+  //             Sınav sonucuna göre harf notu: {getGrades(item)} - Name:
+  //             {item.name} - Grade: {item.grade}
+  //           </p>
+  //         </div>
+  //       );
+  //     })}
+  //   </div>
+  // );
 }
 
 export default App;
