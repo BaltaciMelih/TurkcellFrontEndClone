1e6345608b9a5b7f184038c19202d78c11c9d332
merkaya1 <110626800+merkaya1@users.noreply.github.com>
2022-12-08 14:25:00 +0300

    React CounterApp(hooks) and React Todolist App start

--- Ogrenciler/mustafaErkaya/React-App/my-app/src/App.js
+++ Ogrenciler/mustafaErkaya/React-App/my-app/src/App.js
@@ -6,13 +6,13 @@
 // import Button from './components/Button';
 // import Tab from './components/Tab';
 // import HelloWorld from "./components/HelloWorld";
-import ReactCC from "./components/ReactCC";
-import ReactFC from "./components/ReactFC";
-import { useState } from "react";
-import { useEffect } from "react";
-import UserInfo from './components/UserInfo'
-
-
+// import ReactCC from "./components/ReactCC";
+// import ReactFC from "./components/ReactFC";
+// import { useState } from "react";
+// import { useEffect } from "react";
+// import UserInfo from './components/UserInfo'
+// import { Button } from 'react-bootstrap';
+import {InputGroup, Form} from 'react-bootstrap'
 
 
 
@@ -25,28 +25,63 @@ import UserInfo from './components/UserInfo'
 function App() {
   // const todos = ["todo1", "todo2", "todo3"]
 
-  const [data, setData] = useState({})
+  // const [data, setData] = useState({})
 
-  useEffect(() => {
-    setData({
-      name: "Mustafa",
-      age: 25,
-      job: "Frontend Dev."
-    })
-  }, [])
+  // useEffect(() => {
+  //   setData({
+  //     name: "Mustafa",
+  //     age: 25,
+  //     job: "Frontend Dev."
+  //   })
+  // }, [])
 
-  console.log(data)
+  // console.log(data)
+
+
+
+// ******  Countere //***** */
+
+  // const [ counter, setCounter] =useState(0)
+
+  //  useEffect(() =>{
+  //   console.log(counter);
+  //  },[counter])
+
+  // ********* TodoList 
+
+
+
+
+
+
 
   return (
-    <div className="App">
+    <div className="App d-flex flex-column justify-content-center align-items-center mt-5">
+      
+      <h1>Todo List</h1>
 
-        <UserInfo data={data} setData={setData}/>
+      <InputGroup className="mb-3">
+        <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+        <Form.Control
+          placeholder="Username"
+          aria-label="Username"
+          aria-describedby="basic-addon1"
+        />
+       </InputGroup>
+   
+
+   {/* <div style={{fontSize: 50}}>{counter}</div>
+    <button onClick={() => setCounter(counter +1)}>Increment</button>
+    <button onClick={() => setCounter(counter -1)}>Increment</button>
+    <Button>Hello</Button> */}
+
+        {/* <UserInfo data={data} setData={setData}/>
 
         <p>selamun aleyküm</p>
         <ReactFC status={'iyi bir şekilde'} style={{background: "red" , color: "white"}}/>
         <ReactCC status={'çöp bu'} />
         <ReactFC/>
-        <ReactCC/>
+        <ReactCC/> */}
        
     
       
@@ -81,6 +116,8 @@ function App() {
       {/* </main> */}
       {/* <img src="/logo192.png" alt="" /> */}
       {/* <img src={logo} alt="" /> */}
+     
+     
      </div>
   );
 }
