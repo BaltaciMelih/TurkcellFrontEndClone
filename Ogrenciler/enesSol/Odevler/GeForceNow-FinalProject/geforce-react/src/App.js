d1fa7754aa84b5c7b5f0a4d9d8f01f578d4b90e7
enessol <94052771+enessol@users.noreply.github.com>
2022-12-09 18:01:30 +0300

    cleaned extra files
    cleaned extra files

--- Ogrenciler/enesSol/Odevler/GeForceNow-FinalProject/geforce-react/src/App.js
+++ Ogrenciler/enesSol/Odevler/GeForceNow-FinalProject/geforce-react/src/App.js
@@ -1,7 +1,8 @@
-import "./App.css";
+import "./App.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <h1>GeForce NOW</h1>;
+  return <h1 className="display-1 mx-0">GeForce NOW</h1>;
 }
 
 export default App;
