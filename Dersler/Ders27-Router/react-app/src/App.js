c9a907e9d2f04f183b6fe52f0ed20638824623e9
Varol Maksutoglu <varolmaksutoglu@yandex.com>
2022-12-09 21:27:34 +0300

    classcomponent

--- Dersler/Ders27-Router/react-app/src/App.js
+++ Dersler/Ders27-Router/react-app/src/App.js
@@ -1,27 +1,33 @@
 import "./App.css";
-import { useRef, useEffect } from "react";
+import { Link } from "react-router-dom";
+// import { useRef, useEffect } from "react";
+// import Portal from "./Portal";
+// import ClassComponent from "./ClassComponent";
 
 function App() {
-  const myCustomInput = useRef();
-  const itemsRef = useRef([]);
-  useEffect(() => {
-    if (myCustomInput.current) {
-      myCustomInput.current.addEventListener("focus", () => {
-        // console.log("Focus oldu");
-        // console.log(myCustomInput.current);
-        console.log(myCustomInput.current.className);
-      });
-      myCustomInput.current.addEventListener("blur", () =>
-        console.log("Blur oldu")
-      );
-    }
-  }, [myCustomInput]);
-  useEffect(() => {
-    console.log("Bir kere çalıştı");
-  }, []);
+  // const myCustomInput = useRef();
+  // const itemsRef = useRef([]);
+  // useEffect(() => {
+  //   if (myCustomInput.current) {
+  //     myCustomInput.current.addEventListener("focus", () => {
+  //       // console.log("Focus oldu");
+  //       // console.log(myCustomInput.current);
+  //       console.log(myCustomInput.current.className);
+  //     });
+  //     myCustomInput.current.addEventListener("blur", () =>
+  //       console.log("Blur oldu")
+  //     );
+  //   }
+  // }, [myCustomInput]);
+  // useEffect(() => {
+  //   console.log("Bir kere çalıştı");
+  // }, []);
+  // const [target, setTarget] = useState(document.body);
+  // const [targetText, setTargetText] = useState("Body");
 
   return (
     <div className="App">
+      {/* <div id="test1"></div>
       <input
         type="text"
         name="text"
@@ -45,6 +51,24 @@ function App() {
       >
         Deneme
       </button>
+      <hr />
+      <div id="test2"></div>
+
+      <button
+        onClick={() => {
+          if (targetText === "Test 1") {
+            setTargetText("Text 2");
+            setTarget(document.getElementById("test2"));
+          } else {
+            setTargetText("Text 1");
+            setTarget(document.getElementById("test1"));
+          }
+        }}
+      >
+        Target Değiştir
+      </button> */}
+      {/* <Portal text={targetText} target={target} /> */}
+      {/* <ClassComponent text="Bu bir deneme yazısı" /> */}
     </div>
   );
 }
