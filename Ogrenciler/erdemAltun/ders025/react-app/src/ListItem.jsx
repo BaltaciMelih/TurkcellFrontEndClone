a3984830010e9c2fa5b6910deac2570ad79edfa1
Erdem-Altun <102313137+Erdem-Altun@users.noreply.github.com>
2022-12-09 20:37:35 +0300

    lessons

---
+++ Ogrenciler/erdemAltun/ders025/react-app/src/ListItem.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ListItem({ student }) {
+  return <li>{student}</li>;
+}
+
+export default function InputValue ({value}){
+    console.log("tıkalndı")
+}
