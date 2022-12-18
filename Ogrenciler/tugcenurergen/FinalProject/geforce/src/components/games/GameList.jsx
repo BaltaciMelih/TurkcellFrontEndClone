9306cc9cb8b16a3a4fdf865646f61669d5c44244
Tugcenrgn <tugcenurergen9@gmail.com>
2022-12-19 02:33:08 +0300

    final update

--- Ogrenciler/tugcenurergen/FinalProject/geforce/src/components/games/GameList.jsx
+++ Ogrenciler/tugcenurergen/FinalProject/geforce/src/components/games/GameList.jsx
@@ -1,18 +1,52 @@
 import React, { Component } from "react";
-import GameCard from "./GameCard";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as gameActions from "../../redux/actions/gameActions";
+import { Row, Col } from "react-bootstrap";
+import polygon from "../../assets/polygon.svg";
+import "./GameList.css";
 
 class GameList extends Component {
   componentDidMount() { //componentler yüklendikten sonra oyunları çeker
     this.props.actions.getGames();
   }
+  alphabet =[];
+   
+  
 
   render() { //games'i gameCard componentine gönderir ve oyunların ekrana yazdırılmasını sağlar
     return (
-      <div>
-        <GameCard gamelist={this.props.games} /> 
+      <div >
+        {this.props.games.map((game) => {
+          this.alphabet.push(game.title[0]);
+        })}
+        <div className="d-none">{(this.alphabet = [...new Set(this.alphabet.sort())])}</div>
+        {this.alphabet.map((letter, index) => (
+          <div className="bg-primary m-auto mb-5 p-4 pt-3 ps-1">
+            <Row className="w-100 m-auto" key={index}>
+              <Col className="p-4 ps-4 polygon-col ">
+                <img src={polygon} alt="polygon" className="polygon" />
+                <span className="polygon-letter">{letter}</span>
+              </Col>
+            </Row>
+            <Row className="w-100 m-auto ">
+              {this.props.games.map((game, index) => {
+                if (game.title[0] === letter) {
+                  return (
+                    <Col
+                      key={index}
+                      className="col-12 col-md-6  ps-4  text-light ">
+                      {game.title}
+                    </Col>
+                  );
+                }
+                else{
+                  
+                }
+              })}
+            </Row>
+          </div>
+        ))}
       </div>
     );
   }
