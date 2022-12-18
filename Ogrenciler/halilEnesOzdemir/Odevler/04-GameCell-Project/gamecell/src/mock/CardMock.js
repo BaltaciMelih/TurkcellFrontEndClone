456c96e0db290d64cfbcfbbabf8ffaf9b7e05748
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-12-19 01:20:58 +0300

    Strange changes header and features and corrected, some ui styling behavior has changed.

--- Ogrenciler/halilEnesOzdemir/Odevler/04-GameCell-Project/gamecell/src/mock/CardMock.js
+++ Ogrenciler/halilEnesOzdemir/Odevler/04-GameCell-Project/gamecell/src/mock/CardMock.js
@@ -4,17 +4,17 @@ const datas = [
   {
     title: 'Gameplus ile ayrıcalıklı GeForce Now deneyimine hoşgeldin!',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempor augue vel porta ullamcorper.',
-    button: <Button children={'REGISTER'} width='11.56rem' height='2.5rem' />
+    button: <Button children={'REGISTER'} width='11.56rem' height='2.5rem' className={`border-0`} />
   },
   {
     title: 'Gameplus ile ayrıcalıklı GeForce Now deneyimine hoşgeldin!',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempor augue vel porta ullamcorper.',
-    button: <Button children={'REGISTER'} width='11.56rem' height='2.5rem' />
+    button: <Button children={'REGISTER'} width='11.56rem' height='2.5rem' className={`border-0`} />
   },
   {
     title: 'Gameplus ile ayrıcalıklı GeForce Now deneyimine hoşgeldin!',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempor augue vel porta ullamcorper.',
-    button: <Button children={'REGISTER'} width='11.56rem' height='2.5rem' />
+    button: <Button children={'REGISTER'} width='11.56rem' height='2.5rem' className={`border-0`} />
   }
 ];
 
