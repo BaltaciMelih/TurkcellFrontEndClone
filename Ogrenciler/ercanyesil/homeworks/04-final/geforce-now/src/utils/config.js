419d7c1d2977e7741742c789d6c70ec16b4024f7
Ercan <ercanyesill@outlook.com>
2022-12-18 23:48:48 +0300

    Final Project

---
+++ Ogrenciler/ercanyesil/homeworks/04-final/geforce-now/src/utils/config.js
@@ -0,0 +1,66 @@
+import game1 from "../assets/games/gfn-spotlight-world-of-warships-fb-ig-2048-x-2048.png"
+import game2 from "../assets/games/gfn-spotlight-world-of-warships-fb-ig-2048-x-2048_10.png"
+import game3 from "../assets/games/gfn-spotlight-world-of-warships-fb-ig-2048-x-2048_11.png"
+import game4 from "../assets/games/gfn-spotlight-world-of-warships-fb-ig-2048-x-2048_12.png"
+import game5 from "../assets/games/gfn-spotlight-world-of-warships-fb-ig-2048-x-2048_2.png"
+import game6 from "../assets/games/gfn-spotlight-world-of-warships-fb-ig-2048-x-2048_3.png"
+import game7 from "../assets/games/gfn-spotlight-world-of-warships-fb-ig-2048-x-2048_4.png"
+import game8 from "../assets/games/gfn-spotlight-world-of-warships-fb-ig-2048-x-2048_5.png"
+import game9 from "../assets/games/gfn-spotlight-world-of-warships-fb-ig-2048-x-2048_6.png"
+import game10 from "../assets/games/gfn-spotlight-world-of-warships-fb-ig-2048-x-2048_7.png"
+import game11 from "../assets/games/gfn-spotlight-world-of-warships-fb-ig-2048-x-2048_8.png"
+import game12 from "../assets/games/gfn-spotlight-world-of-warships-fb-ig-2048-x-2048_9.png"
+export const homeRail=
+     [
+    {
+        img: game1,
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlgKj2F2Ub7Wex7vfAPFW2J3mUYTyntz55BxrILK8F&s"
+    },
+    {
+        img: game2,
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlgKj2F2Ub7Wex7vfAPFW2J3mUYTyntz55BxrILK8F&s"
+    },
+    {
+        img: game3,
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlgKj2F2Ub7Wex7vfAPFW2J3mUYTyntz55BxrILK8F&s"
+    },
+    {
+        img: game4,
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlgKj2F2Ub7Wex7vfAPFW2J3mUYTyntz55BxrILK8F&s"
+    },
+    {
+        img: game5,
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlgKj2F2Ub7Wex7vfAPFW2J3mUYTyntz55BxrILK8F&s"
+    },
+    {
+        img: game6,
+        url: "https://avatars.mds.yandex.net/i?id=e67c20f98bdc512c5d3bc20c140f8fac-5719595-images-taas-consumers&n=27&h=384&w=480"
+    }
+    ]
+export const homeRail2=
+     [
+    {
+        img: game7,
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlgKj2F2Ub7Wex7vfAPFW2J3mUYTyntz55BxrILK8F&s"
+    },
+    {
+        img: game8,
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlgKj2F2Ub7Wex7vfAPFW2J3mUYTyntz55BxrILK8F&s"
+    },
+    {
+        img: game9,
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlgKj2F2Ub7Wex7vfAPFW2J3mUYTyntz55BxrILK8F&s"
+    },
+    {
+        img: game10,
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlgKj2F2Ub7Wex7vfAPFW2J3mUYTyntz55BxrILK8F&s"
+    },
+    {
+        img: game11,
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlgKj2F2Ub7Wex7vfAPFW2J3mUYTyntz55BxrILK8F&s"
+    },
+    {
+        img: game12,
+        url: "https://avatars.mds.yandex.net/i?id=e67c20f98bdc512c5d3bc20c140f8fac-5719595-images-taas-consumers&n=27&h=384&w=480"
+    }
+    ]
