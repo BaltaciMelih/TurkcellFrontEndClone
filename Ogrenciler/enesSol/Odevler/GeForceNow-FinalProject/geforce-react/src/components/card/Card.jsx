00d010c4b5a923f0d579b710975a08d48d3afee5
enessol <94052771+enessol@users.noreply.github.com>
2022-12-18 22:54:39 +0300

    Turkcell Front-End Final Project Completed
    Turkcell Gameplus GeForce Now clone final project completed.

---
+++ Ogrenciler/enesSol/Odevler/GeForceNow-FinalProject/geforce-react/src/components/card/Card.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Card.scss";
+
+export default function Card(props) {
+  return (
+    <div>
+      <div className="game-titles-bg mb-4 p-4">
+        <div className="games-hex text-center mb-4">
+          <p className="py-1 fs-2 text-dark fw-bold">{props.item[0]}</p>
+        </div>
+        <div className="content-area d-flex flex-wrap">
+          {props.item[1].map((val, index) => (
+            <a target="_blank" key={index} href={val.url} className="game-list">
+              {val.title}
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
