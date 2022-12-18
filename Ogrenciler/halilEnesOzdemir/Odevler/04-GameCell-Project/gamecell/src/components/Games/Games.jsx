1ecccda329f9fe11431d4627ce92237d3bb4ff91
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-12-18 16:10:09 +0300

    Games part OK. D-none problem solved, button's set.

--- Ogrenciler/halilEnesOzdemir/Odevler/04-GameCell-Project/gamecell/src/components/Games/Games.jsx
+++ Ogrenciler/halilEnesOzdemir/Odevler/04-GameCell-Project/gamecell/src/components/Games/Games.jsx
@@ -5,8 +5,8 @@ import './Games.scss';
 function Games({ gameImage, gameImageDesktop }) {
   return (
     <>
-      <div className='container-fluid bg-black py-2 '>
-        <p className='text-center h3 games-text mb-4'>Choose the game you want to play</p>
+      <div className='container-fluid bg-black py-2 py-lg-5 '>
+        <p className='text-center h2 games-text mb-lg-5 mb-4'>Choose the game you want to play</p>
         <div className='row mb-3'>
           {gameImage.map((game, index) => {
             return (
@@ -20,21 +20,21 @@ function Games({ gameImage, gameImageDesktop }) {
           children={'SEE ALL GAMES'}
           width='159px'
           height='48px'
-          className={`d-block mx-auto d-lg-none  `}></Button>
-        <div className='row  '>
+          className={`d-block mx-auto d-lg-none border-0  `}></Button>
+        <div className='row'>
           {gameImageDesktop.map((game, index) => {
             return (
-              <div key={index} className='col-6  col-sm-4 col-lg-2 mb-3'>
-                <img src={game.src} className=' img-fluid  w-100 d-none d-lg-block' alt='...' />
+              <div key={index} className='col-6  col-sm-4 col-lg-2 mb-4 d-none  d-lg-block'>
+                <img src={game.src} className=' img-fluid  w-100 ' alt='...' />
               </div>
             );
           })}
         </div>
         <Button
           children={'SEE ALL GAMES'}
-          width='159px'
-          height='48px'
-          className={`d-block mx-auto d-none d-lg-block`}></Button>
+          width='185px'
+          height='40px'
+          className={`d-block mx-auto d-none d-lg-block border-0`}></Button>
       </div>
     </>
   );
