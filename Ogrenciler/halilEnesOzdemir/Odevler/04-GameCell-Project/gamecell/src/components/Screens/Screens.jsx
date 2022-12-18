ec5f09f6af0306aef943c6151c74c76b99f39838
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-12-18 16:00:31 +0300

    Screens part handled, not baaaad not baaad :)

--- Ogrenciler/halilEnesOzdemir/Odevler/04-GameCell-Project/gamecell/src/components/Screens/Screens.jsx
+++ Ogrenciler/halilEnesOzdemir/Odevler/04-GameCell-Project/gamecell/src/components/Screens/Screens.jsx
@@ -5,9 +5,9 @@ function Screens() {
   return (
     <>
       <div className='container-fluid screen-bg text-white text-center '>
-        <h1 className='fs-5 py-2 px-4'>On all your screens at home and on the go</h1>
+        <h1 className='fs-3 py-2 px-sm-4'>On all your screens at home and on the go</h1>
         <div className=' container-sm mx-auto row row-cols-1 row-cols-lg-3 d-flex flex-lg-row flex-wrap flex-lg-nowrap  px-2 py-3 gap-3 gap-lg-3 screens  px-1 px-sm-3 py-lg-5 px-lg-5'>
-          <div className='border py-2 py-lg-4  screen-border d-flex justify-content-start justify-content-lg-center align-items-center gap-2  '>
+          <div className=' py-2  py-lg-4  screen-border d-flex justify-content-start justify-content-lg-center align-items-center gap-2  '>
             <img
               className='  d-inline-block screen-icon ms-2 me-3 screen-image-1  '
               src='https://cdn.zeplin.io/5f91645bc7d3b352a5fe0ebd/assets/B5E506C1-478B-43E7-A192-A8EDCD2222F7.svg'
@@ -16,7 +16,7 @@ function Screens() {
             <span>Available on Windows</span>
           </div>
 
-          <div className=' border py-2 py-lg-3  screen-border d-flex justify-content-start justify-content-lg-center  align-items-center gap-2  '>
+          <div className='  py-2 py-lg-3  screen-border d-flex justify-content-start justify-content-lg-center  align-items-center gap-2  '>
             <img
               className='d-inline-block screen-icon ms-2 me-3 screen-image-2'
               src='https://cdn.zeplin.io/5f91645bc7d3b352a5fe0ebd/assets/B9F2C037-179B-4C7E-B3B1-D7440F430386.svg'
@@ -24,7 +24,7 @@ function Screens() {
             />
             <span>Available on Mac</span>
           </div>
-          <div className=' border py-2 py-lg-3 screen-border d-flex justify-content-start justify-content-lg-center  align-items-center gap-2 '>
+          <div className=' py-2 py-lg-3 screen-border d-flex justify-content-start justify-content-lg-center  align-items-center gap-2 '>
             <img
               className='d-inline-block screen-icon ms-2 me-3 screen-image-3  '
               src='https://cdn.zeplin.io/5f91645bc7d3b352a5fe0ebd/assets/FF527D5C-B6BD-4D1E-81ED-11062B23BF5B.svg'
